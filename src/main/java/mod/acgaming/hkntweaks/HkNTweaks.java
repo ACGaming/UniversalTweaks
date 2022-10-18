fea86be830e30ecb120aa28e1fbf9638d5e05519
ACGaming <4818419+ACGaming@users.noreply.github.com>
2022-10-18 19:55:23 +0200

    Stronghold replacement
    Courtesy of yungnickyoung

--- src/main/java/mod/acgaming/hkntweaks/HkNTweaks.java
+++ src/main/java/mod/acgaming/hkntweaks/HkNTweaks.java
@@ -2,12 +2,16 @@ package mod.acgaming.hkntweaks;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 import mod.acgaming.hkntweaks.config.HkNTweaksConfig;
 import mod.acgaming.hkntweaks.tweaks.HkNAttributes;
+import mod.acgaming.hkntweaks.tweaks.stronghold.HkNStronghold;
+import mod.acgaming.hkntweaks.tweaks.stronghold.worldgen.SafeStrongholdWorldGenerator;
 
 @Mod(modid = HkNTweaks.MODID,
     name = HkNTweaks.NAME,
@@ -26,11 +30,13 @@ public class HkNTweaks
     public void preInit(FMLPreInitializationEvent event)
     {
         if (HkNTweaksConfig.tweaks.hknAttributesToggle) HkNAttributes.hknSetAttributes();
+        if (HkNTweaksConfig.tweaks.hknStrongholdToggle) GameRegistry.registerWorldGenerator(new SafeStrongholdWorldGenerator(), Integer.MAX_VALUE);
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
+        if (HkNTweaksConfig.tweaks.hknStrongholdToggle) MinecraftForge.TERRAIN_GEN_BUS.register(new HkNStronghold());
         LOGGER.info("HkN Tweaks initialized");
     }
 }
--- src/main/java/mod/acgaming/hkntweaks/config/HkNTweaksConfig.java
+++ src/main/java/mod/acgaming/hkntweaks/config/HkNTweaksConfig.java
@@ -21,22 +21,27 @@ public class HkNTweaksConfig
 
     public static class BugfixesCategory
     {
+        @Config.RequiresMcRestart
         @Config.Name("Block Overlay Fix")
         @Config.Comment("Fixes x-ray when standing in non-suffocating blocks")
         public boolean hknBlockOverlayToggle = true;
 
+        @Config.RequiresMcRestart
         @Config.Name("Dimension Change Player States Fix")
         @Config.Comment("Fixes missing player states when changing dimensions by sending additional packets")
         public boolean hknDimensionChangeToggle = true;
 
+        @Config.RequiresMcRestart
         @Config.Name("Entity Bounding Box Fix")
         @Config.Comment("Saves entity bounding boxes to tags to prevent breakouts and suffocation")
         public boolean hknEntityAABBToggle = true;
 
+        @Config.RequiresMcRestart
         @Config.Name("Ladder Flying Slowdown Fix")
         @Config.Comment("Disables climbing movement when flying")
         public boolean hknLadderFlyingToggle = true;
 
+        @Config.RequiresWorldRestart
         @Config.Name("Max Health Fix")
         @Config.Comment("Saves increased player health to tags")
         public boolean hknMaxHealthToggle = true;
@@ -45,14 +50,17 @@ public class HkNTweaksConfig
         @Config.Comment("Only repairs damaged equipment with XP")
         public boolean hknMendingToggle = true;
 
+        @Config.RequiresMcRestart
         @Config.Name("Piston Progress Fix")
         @Config.Comment("Properly saves the last state of pistons to tags")
         public boolean hknPistonTileToggle = true;
 
+        @Config.RequiresMcRestart
         @Config.Name("Skeleton Aim Fix")
         @Config.Comment("Fixes skeletons not looking at their targets when strafing")
         public boolean hknSkeletonAimToggle = true;
 
+        @Config.RequiresMcRestart
         @Config.Name("Tile Entity Update Order Fix")
         @Config.Comment("Keeps the order of tile entities on chunk load")
         public boolean hknTELoadOrderToggle = true;
@@ -60,14 +68,17 @@ public class HkNTweaksConfig
 
     public static class TweaksCategory
     {
+        @Config.RequiresMcRestart
         @Config.Name("Attributes Uncap")
         @Config.Comment("Virtually uncaps entity attributes")
         public boolean hknAttributesToggle = true;
 
+        @Config.RequiresMcRestart
         @Config.Name("Auto Jump Replacement")
         @Config.Comment("Replaces auto jump with an increased step height")
         public boolean hknAutoJumpToggle = true;
 
+        @Config.RequiresMcRestart
         @Config.Name("Bed Obstruction Replacement")
         @Config.Comment("Replaces bed obstruction checks with an improved version")
         public boolean hknBedObstructionToggle = true;
@@ -76,14 +87,17 @@ public class HkNTweaksConfig
         @Config.Comment("Bows enchanted with Infinity no longer require arrows")
         public boolean hknBowInfinityToggle = true;
 
+        @Config.RequiresMcRestart
         @Config.Name("Water Fall Damage")
         @Config.Comment("Re-implements an improved version of pre-1.4 fall damage in water")
         public boolean hknFallDamageToggle = false;
 
+        @Config.RequiresMcRestart
         @Config.Name("Water Fall Damage Reduction")
         @Config.Comment("How much fall damage gets reduced by water per tick")
         public float hknFallDamageValue = 2F;
 
+        @Config.RequiresMcRestart
         @Config.Name("Mob Despawn Improvement")
         @Config.Comment("Mobs carrying picked up items will drop their equipment and despawn properly")
         public boolean hknMobDespawnToggle = true;
@@ -91,6 +105,11 @@ public class HkNTweaksConfig
         @Config.Name("Offhand Improvement")
         @Config.Comment("Prevents placing offhand blocks when blocks or food are held in the mainhand")
         public boolean hknOffhandToggle = true;
+
+        @Config.RequiresMcRestart
+        @Config.Name("Stronghold Replacement")
+        @Config.Comment("Replaces stronghold generation with a safer variant")
+        public boolean hknStrongholdToggle = true;
     }
 
     @Mod.EventBusSubscriber(modid = HkNTweaks.MODID)
