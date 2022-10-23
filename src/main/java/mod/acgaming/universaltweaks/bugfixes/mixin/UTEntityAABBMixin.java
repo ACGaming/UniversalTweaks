35c200e22a5340ae9fb0aac5fd9920015117a50e
ACGaming <4818419+ACGaming@users.noreply.github.com>
2022-10-23 15:01:02 +0200

    Switch entity AABB fix

--- src/main/java/mod/acgaming/universaltweaks/bugfixes/mixin/UTEntityAABBMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/bugfixes/mixin/UTEntityAABBMixin.java
@@ -4,7 +4,6 @@ import net.minecraft.entity.Entity;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraftforge.common.util.Constants;
 
 import mod.acgaming.universaltweaks.UniversalTweaks;
 import mod.acgaming.universaltweaks.config.UTConfig;
@@ -12,39 +11,65 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 // MC-2025
 // https://bugs.mojang.com/browse/MC-2025
+// Courtesy of mrgrim
 @Mixin(Entity.class)
 public abstract class UTEntityAABBMixin
 {
+    @Shadow
+    public double posX;
+    @Shadow
+    public double posY;
+    @Shadow
+    public double posZ;
+
     @Shadow
     public abstract AxisAlignedBB getEntityBoundingBox();
 
     @Shadow
     public abstract void setEntityBoundingBox(AxisAlignedBB bb);
 
-    @Inject(method = "writeToNBT", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/NBTTagCompound;setTag(Ljava/lang/String;Lnet/minecraft/nbt/NBTBase;)V", ordinal = 2))
-    public void utWriteAABBToNBT(NBTTagCompound compound, CallbackInfoReturnable<NBTTagCompound> cir)
-    {
-        if (UTConfig.debug.utDebugToggle) UniversalTweaks.LOGGER.debug("UTEntityAABB ::: Write AABB to NBT");
-        AxisAlignedBB aabb = this.getEntityBoundingBox();
-        if (aabb != null) compound.setTag("AABB", newDoubleNBTList(aabb.minX, aabb.minY, aabb.minZ, aabb.maxX, aabb.maxY, aabb.maxZ));
-    }
-
-    @Inject(method = "readFromNBT", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;setRotation(FF)V"))
-    public void utReadAABBFromNBT(NBTTagCompound compound, CallbackInfo ci)
-    {
-        if (UTConfig.debug.utDebugToggle) UniversalTweaks.LOGGER.debug("UTEntityAABB ::: Read AABB from NBT");
-        if (compound.hasKey("AABB"))
-        {
-            NBTTagList aabbNbt = compound.getTagList("AABB", Constants.NBT.TAG_DOUBLE);
-            this.setEntityBoundingBox(new AxisAlignedBB(aabbNbt.getDoubleAt(0), aabbNbt.getDoubleAt(1), aabbNbt.getDoubleAt(2), aabbNbt.getDoubleAt(3), aabbNbt.getDoubleAt(4), aabbNbt.getDoubleAt(5)));
-        }
-    }
+    @Shadow
+    public abstract void setPosition(double x, double y, double z);
 
     @Shadow
     protected abstract NBTTagList newDoubleNBTList(double... numbers);
+
+    @Shadow
+    protected abstract boolean shouldSetPosAfterLoading();
+
+    @Inject(method = "writeToNBT", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/NBTTagCompound;setUniqueId(Ljava/lang/String;Ljava/util/UUID;)V", shift = At.Shift.AFTER, ordinal = 0))
+    private void utSaveAABBToNBT(NBTTagCompound compound, CallbackInfoReturnable<NBTTagCompound> ci)
+    {
+        if (UTConfig.bugfixes.utEntityAABBToggle)
+        {
+            if (UTConfig.debug.utDebugToggle) UniversalTweaks.LOGGER.debug("UTEntityAABB ::: Write AABB to NBT");
+            AxisAlignedBB aabb = this.getEntityBoundingBox();
+            compound.setTag("AABB", this.newDoubleNBTList(aabb.minX, aabb.minY, aabb.minZ, aabb.maxX, aabb.maxY, aabb.maxZ));
+        }
+    }
+
+    @Redirect(method = "readFromNBT", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;shouldSetPosAfterLoading()Z"))
+    private boolean utReadAABBFromNBT(Entity entity, NBTTagCompound compound)
+    {
+        if (this.shouldSetPosAfterLoading()) this.setPosition(this.posX, this.posY, this.posZ);
+        if (UTConfig.bugfixes.utEntityAABBToggle && compound.hasKey("AABB", 9))
+        {
+            if (UTConfig.debug.utDebugToggle) UniversalTweaks.LOGGER.debug("UTEntityAABB ::: Read AABB from NBT");
+            NBTTagList aabb_tag = compound.getTagList("AABB", 6);
+            AxisAlignedBB aabb = new AxisAlignedBB(aabb_tag.getDoubleAt(0), aabb_tag.getDoubleAt(1), aabb_tag.getDoubleAt(2), aabb_tag.getDoubleAt(3), aabb_tag.getDoubleAt(4), aabb_tag.getDoubleAt(5));
+            double deltaX = ((aabb.minX + aabb.maxX) / 2.0D) - this.posX;
+            double deltaY = aabb.minY - this.posY;
+            double deltaZ = ((aabb.minZ + aabb.maxZ) / 2.0D) - this.posZ;
+            // If the position and AABB center point are > 0.1 blocks apart then do not restore the AABB. In vanilla
+            // this should never happen, but mods might not be aware that the AABB is stored and that the entity
+            // position will be reset to it. A good example of this is the ExtraUtils2 golden lasso.
+            if (((deltaX * deltaX) + (deltaY * deltaY) + (deltaZ * deltaZ)) < 0.01D) this.setEntityBoundingBox(aabb);
+        }
+        return false;
+    }
 }
