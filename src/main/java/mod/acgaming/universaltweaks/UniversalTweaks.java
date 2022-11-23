50ef47b49345513c1d4438a8ba03f44ce3b3b033
ACGaming <4818419+ACGaming@users.noreply.github.com>
2022-11-23 18:58:38 +0100

    Handle Ender IO obsolete mods screen edge case
    Closes #4

--- src/main/java/mod/acgaming/universaltweaks/UniversalTweaks.java
+++ src/main/java/mod/acgaming/universaltweaks/UniversalTweaks.java
@@ -32,7 +32,7 @@ public class UniversalTweaks
 {
     public static final String MODID = "universaltweaks";
     public static final String NAME = "Universal Tweaks";
-    public static final String VERSION = "1.12.2-1.0.0-pre3";
+    public static final String VERSION = "1.12.2-1.0.0-pre4";
     public static final String DEPENDENCIES = "required-after:mixinbooter;" +
         "before:attributefix;" +
         "before:bedbreakbegone;" +
@@ -84,7 +84,7 @@ public class UniversalTweaks
         if (Loader.isModLoaded("unloader")) messages.add("Unloader");
         if (messages.size() > 3)
         {
-            if (FMLLaunchHandler.side() == Side.CLIENT && UTConfig.debug.utObsoleteModsToggle) UTObsoleteModsHandler.throwException(messages);
+            if (FMLLaunchHandler.side() == Side.CLIENT && UTConfig.debug.utObsoleteModsToggle && !Loader.isModLoaded("enderio")) UTObsoleteModsHandler.throwException(messages);
             else
             {
                 messages.add("");
