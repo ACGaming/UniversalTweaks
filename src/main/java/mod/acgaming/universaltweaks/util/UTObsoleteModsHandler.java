d2f455c3a33886225daeb11f08373ca5814bf8c9
ACGaming <4818419+ACGaming@users.noreply.github.com>
2022-12-16 15:05:20 +0100

    Outsource obsolete mods screen for server compatibility

--- src/main/java/mod/acgaming/universaltweaks/util/UTObsoleteModsHandler.java
+++ src/main/java/mod/acgaming/universaltweaks/util/UTObsoleteModsHandler.java
@@ -3,13 +3,9 @@ package mod.acgaming.universaltweaks.util;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.gui.GuiErrorScreen;
-import net.minecraftforge.fml.client.CustomModLoadingErrorDisplayException;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.relauncher.FMLLaunchHandler;
 import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 import mod.acgaming.universaltweaks.config.UTConfig;
 
@@ -76,43 +72,12 @@ public class UTObsoleteModsHandler
                 && !Loader.isModLoaded("enderio")
                 && !Loader.isModLoaded("gregtech"))
             {
-                UTObsoleteModsHandler.throwException(messages);
+                UTObsoleteModsException.throwException(messages);
             }
             else
             {
                 messages.add("");
                 throw new RuntimeException(String.join(System.lineSeparator(), messages));
-            }
-        }
-    }
-
-    public static void throwException(List<String> messages)
-    {
-        throw new Exception(messages);
-    }
-
-    @SideOnly(Side.CLIENT)
-    private static class Exception extends CustomModLoadingErrorDisplayException
-    {
-        private final List<String> messages;
-
-        public Exception(List<String> messages)
-        {
-            this.messages = messages;
-        }
-
-        @Override
-        public void initGui(GuiErrorScreen errorScreen, FontRenderer fontRenderer) {}
-
-        @Override
-        public void drawScreen(GuiErrorScreen errorScreen, FontRenderer fontRenderer, int mouseRelX, int mouseRelY, float tickTime)
-        {
-            int x = errorScreen.width / 2;
-            int y = 75;
-            for (String message : messages)
-            {
-                errorScreen.drawCenteredString(fontRenderer, message, x, y, 0xFFFFFF);
-                y += 15;
             }
         }
     }
