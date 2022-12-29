769b313931897fad4e222b0a8dd33557e2f68474
ACGaming <4818419+ACGaming@users.noreply.github.com>
2022-12-29 15:20:39 +0100

    Implement TC wisp particles tweak

--- src/main/java/mod/acgaming/universaltweaks/config/UTConfig.java
+++ src/main/java/mod/acgaming/universaltweaks/config/UTConfig.java
@@ -198,6 +198,10 @@ public class UTConfig
         @Config.Comment("Adds particles to firebats similar to legacy versions")
         public boolean utTCFirebatParticlesToggle = true;
 
+        @Config.Name("[Thaumcraft] Wisp Particles")
+        @Config.Comment("Increases particle size of wisps similar to legacy versions")
+        public boolean utTCWispParticlesToggle = true;
+
         @Config.Name("[Tinkers' Construct] Gaseous Fluids")
         @Config.Comment("Excludes gaseous fluids from being transferable via faucets")
         public boolean utTConGaseousFluidsToggle = false;
---
+++ src/main/java/mod/acgaming/universaltweaks/mods/thaumcraft/mixin/UTWispParticlesMixin.java
@@ -0,0 +1,18 @@
+package mod.acgaming.universaltweaks.mods.thaumcraft.mixin;
+
+import mod.acgaming.universaltweaks.config.UTConfig;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import thaumcraft.common.entities.monster.EntityWisp;
+
+@Mixin(EntityWisp.class)
+public class UTWispParticlesMixin
+{
+    @ModifyConstant(method = "onDeath", constant = @Constant(floatValue = 1.0F))
+    public float utWispParticles(float constant)
+    {
+        if (UTConfig.mods.utTCWispParticlesToggle) return 10;
+        else return constant;
+    }
+}
