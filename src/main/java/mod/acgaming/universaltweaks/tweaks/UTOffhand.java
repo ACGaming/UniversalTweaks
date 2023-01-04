88ed2f4d17f3f568c3a645bfb3333d5c5cb39715
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-01-04 20:14:14 +0100

    Extend offhand improvement to shield blocking

--- src/main/java/mod/acgaming/universaltweaks/tweaks/UTOffhand.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/UTOffhand.java
@@ -4,10 +4,12 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemShield;
 import net.minecraft.util.EnumHand;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.Event;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import mod.acgaming.universaltweaks.UniversalTweaks;
@@ -16,8 +18,9 @@ import mod.acgaming.universaltweaks.config.UTConfig;
 @Mod.EventBusSubscriber(modid = UniversalTweaks.MODID)
 public class UTOffhand
 {
-    @SubscribeEvent
-    public static void utOffhand(PlayerInteractEvent.RightClickBlock event)
+    // Don't place blocks in the offhand when blocks or food are in the mainhand
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public static void utOffhandBlock(PlayerInteractEvent.RightClickBlock event)
     {
         if (!UTConfig.tweaks.utOffhandToggle) return;
         if (UTConfig.debug.utDebugToggle) UniversalTweaks.LOGGER.debug("UTOffhand ::: Right click block event");
@@ -28,5 +31,22 @@ public class UTOffhand
             && heldItemOffhand instanceof ItemBlock
             && (heldItemMainhand instanceof ItemBlock || heldItemMainhand instanceof ItemFood))
             event.setUseItem(Event.Result.DENY);
+    }
+
+    // Don't interact with entities when wielding shields
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public static void utOffhandEntity(PlayerInteractEvent.EntityInteract event)
+    {
+        if (!UTConfig.tweaks.utOffhandToggle) return;
+        if (UTConfig.debug.utDebugToggle) UniversalTweaks.LOGGER.debug("UTOffhand ::: Right click entity event");
+        EntityPlayer player = event.getEntityPlayer();
+        Item heldItemMainhand = player.getHeldItemMainhand().getItem();
+        Item heldItemOffhand = player.getHeldItemOffhand().getItem();
+        if (event.getHand() == EnumHand.MAIN_HAND && heldItemOffhand instanceof ItemShield
+            || event.getHand() == EnumHand.OFF_HAND && heldItemMainhand instanceof ItemShield)
+        {
+            event.setResult(Event.Result.DENY);
+            event.setCanceled(true);
+        }
     }
 }
