36073c770e28fe1a83337fe12317625720a11861
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-01-30 18:54:30 +0100

    Improve hook of skip credits tweak

--- src/main/java/mod/acgaming/universaltweaks/tweaks/mixin/UTSkipCreditsMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/mixin/UTSkipCreditsMixin.java
@@ -4,7 +4,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.network.play.client.CPacketClientStatus;
 import net.minecraft.network.play.server.SPacketChangeGameState;
-import net.minecraft.util.math.MathHelper;
 
 import mod.acgaming.universaltweaks.UniversalTweaks;
 import mod.acgaming.universaltweaks.config.UTConfig;
@@ -20,12 +19,12 @@ public class UTSkipCreditsMixin
     @Shadow
     private Minecraft client;
 
-    @Inject(method = "handleChangeGameState", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;floor(F)I", shift = At.Shift.AFTER), cancellable = true)
+    @Inject(method = "handleChangeGameState", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;displayGuiScreen(Lnet/minecraft/client/gui/GuiScreen;)V", ordinal = 1), cancellable = true)
     public void utSkipCredits(SPacketChangeGameState packetIn, CallbackInfo ci)
     {
         if (!UTConfig.TWEAKS_MISC.utSkipCreditsToggle) return;
         if (UTConfig.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTSkipCredits ::: Handle change game state");
-        if (packetIn.getGameState() == 4 && MathHelper.floor(packetIn.getValue() + 0.5F) == 1) this.client.player.connection.sendPacket(new CPacketClientStatus(CPacketClientStatus.State.PERFORM_RESPAWN));
+        this.client.player.connection.sendPacket(new CPacketClientStatus(CPacketClientStatus.State.PERFORM_RESPAWN));
         ci.cancel();
     }
 }
