2658de7c09c6e0cce0358235e6077912f52b30fa
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-02-10 21:28:55 +0100

    Micro-optimize atan2

--- src/main/java/mod/acgaming/universaltweaks/tweaks/ai/UTDiamondAtan2.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/ai/UTDiamondAtan2.java
@@ -3,22 +3,11 @@ package mod.acgaming.universaltweaks.tweaks.ai;
 // https://stackoverflow.com/questions/1427422/cheap-algorithm-to-find-measure-of-angle-between-vectors/14675998#14675998
 public class UTDiamondAtan2
 {
-    public static final double PI_2 = 1.5707963;
-
     public static double atan2(double y, double x)
     {
         double angle;
         if (y == 0 && x >= 0) return 0;
-        else if (y >= 0)
-        {
-            if (x >= 0) angle = y / (x + y);
-            else angle = 1 - x / (-x + y);
-        }
-        else
-        {
-            if (x < 0) angle = -2 + y / (x + y);
-            else angle = -1 + x / (x - y);
-        }
-        return angle * PI_2;
+        else angle = (y >= 0) ? (x >= 0 ? y / (x + y) : 1 - x / (-x + y)) : (x < 0 ? -2 + y / (x + y) : -1 + x / (x - y));
+        return angle * 1.5707963;
     }
 }
