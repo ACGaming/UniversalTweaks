d7c86b2ed4f01f58bc362823b0a928245b99801d
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-02-19 08:58:51 +0100

    Replace recipe book button with dummy
    Fixes crashes with mods depending on button ID 10

--- src/main/java/mod/acgaming/universaltweaks/tweaks/recipebook/mixin/UTRecipeBookInvMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/recipebook/mixin/UTRecipeBookInvMixin.java
@@ -1,22 +1,27 @@
 package mod.acgaming.universaltweaks.tweaks.recipebook.mixin;
 
+import net.minecraft.client.gui.GuiButtonImage;
 import net.minecraft.client.gui.inventory.GuiInventory;
+import net.minecraft.util.ResourceLocation;
 
 import mod.acgaming.universaltweaks.UniversalTweaks;
 import mod.acgaming.universaltweaks.config.UTConfig;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(GuiInventory.class)
 public class UTRecipeBookInvMixin
 {
-    @Inject(method = "initGui", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"), cancellable = true)
-    public void utHideRecipeBook(CallbackInfo ci)
+    @Shadow
+    private GuiButtonImage recipeButton;
+
+    @Redirect(method = "initGui", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/inventory/GuiInventory;recipeButton:Lnet/minecraft/client/gui/GuiButtonImage;", ordinal = 0))
+    public void utHideRecipeBook(GuiInventory instance, GuiButtonImage value)
     {
         if (!UTConfig.TWEAKS_MISC.utRecipeBookToggle) return;
         if (UTConfig.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTRecipeBookInv ::: Initialize GUI");
-        ci.cancel();
+        this.recipeButton = new GuiButtonImage(10, 0, 0, 0, 0, 0, 0, 0, new ResourceLocation("textures/gui/container/inventory.png"));
     }
 }
