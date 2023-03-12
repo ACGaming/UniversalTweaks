31878b66d8a07a915c821c47cb503524a524778f
ChaosStrikez <jjchung276@gmail.com>
2023-03-12 16:09:19 -0700

    Implement Roost/CT load time tweak

--- src/main/java/mod/acgaming/universaltweaks/UniversalTweaks.java
+++ src/main/java/mod/acgaming/universaltweaks/UniversalTweaks.java
@@ -36,7 +36,7 @@ public class UniversalTweaks
     public static final String MODID = "universaltweaks";
     public static final String NAME = "Universal Tweaks";
     public static final String VERSION = "1.12.2-1.4.2";
-    public static final String DEPENDENCIES = "required-after:mixinbooter;after:biomesoplenty;after:botania;after:customspawner;after:epicsiegemod;after:extratrees;after:forestry;after:storagedrawers;after:tconstruct;after:thaumcraft";
+    public static final String DEPENDENCIES = "required-after:mixinbooter;after:biomesoplenty;after:botania;after:contenttweaker;after:customspawner;after:epicsiegemod;after:extratrees;after:forestry;after:roost;after:storagedrawers;after:tconstruct;after:thaumcraft";
     public static final Logger LOGGER = LogManager.getLogger(NAME);
 
     @Mod.EventHandler
---
+++ src/main/java/mod/acgaming/universaltweaks/mods/roost/contenttweaker/mixin/UTChickenModuleMixin.java
@@ -0,0 +1,120 @@
+package mod.acgaming.universaltweaks.mods.roost.contenttweaker.mixin;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+import com.setycz.chickens.handler.SpawnType;
+import com.setycz.chickens.registry.ChickensRegistry;
+import com.setycz.chickens.registry.ChickensRegistryItem;
+import com.teamacronymcoders.contenttweaker.ContentTweaker;
+import com.teamacronymcoders.contenttweaker.modules.chickens.ChickenFactory;
+import com.teamacronymcoders.contenttweaker.modules.chickens.ChickenModule;
+import com.teamacronymcoders.contenttweaker.modules.chickens.ChickenRepresentation;
+import com.teamacronymcoders.base.modulesystem.ModuleBase;
+import crafttweaker.CraftTweakerAPI;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import mod.acgaming.universaltweaks.UniversalTweaks;
+import mod.acgaming.universaltweaks.config.UTConfig;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = ChickenModule.class, remap = false)
+public abstract class UTChickenModuleMixin extends ModuleBase{
+
+    @Override
+    public void preInit(FMLPreInitializationEvent event) {
+        super.preInit(event);
+        if (!UTConfig.MOD_INTEGRATION.ROOST.utRoostEarlyRegisterCTChickens) return;
+        if (UTConfig.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTChickenModuleMixin ::: Register chickens (pre-init)");
+        utRegisterChickens();
+    }
+
+    @Inject(method = "init", at = @At(value = "INVOKE", target = "Lcom/teamacronymcoders/contenttweaker/modules/chickens/ChickenFactory;registerChickens()V"), cancellable = true)
+    public void utCTChickenInit(FMLInitializationEvent e, CallbackInfo ci) {
+        if (UTConfig.MOD_INTEGRATION.ROOST.utRoostEarlyRegisterCTChickens) {
+            if (UTConfig.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTChickenModuleMixin ::: Finish setting up chickens (init)");
+            utInitChickens();
+            // ignore ChickenFactory.registerChickens() and use instance methods instead
+            ci.cancel();
+        }
+    }
+
+    // register barebone chickens to registry
+    private void utRegisterChickens() {
+        for (ChickenRepresentation chickenRepresentation : ChickenFactory.CHICKEN_REPRESENTATIONS) {
+            ChickensRegistryItem item = new ChickensRegistryItem(
+                    new ResourceLocation(ContentTweaker.MOD_ID, chickenRepresentation.name),
+                    chickenRepresentation.name,
+                    chickenRepresentation.textureLocation.getInternal(),
+                    ItemStack.EMPTY,
+                    chickenRepresentation.backgroundColor.getIntColor(),
+                    chickenRepresentation.foregroundColor.getIntColor(),
+                    null,
+                    null
+            );
+            ChickensRegistry.register(item);
+        }
+        if (UTConfig.DEBUG.utDebugToggle) {
+            CraftTweakerAPI.logInfo("UTChickenModuleMixin ::: Dumping chicken registry!");
+            for (ChickensRegistryItem chicken : ChickensRegistry.getItems()) {
+                CraftTweakerAPI.logInfo("UTChickenModuleMixin ::: " + chicken.getRegistryName().toString());
+            }
+        }
+    }
+
+    // finish setting up chickens in init, after registries/resources are loaded
+    private void utInitChickens() {
+        for (ChickenRepresentation chickenRepresentation : ChickenFactory.CHICKEN_REPRESENTATIONS) {
+            if (UTConfig.DEBUG.utDebugToggle) {
+                CraftTweakerAPI.logInfo("UTChickenModuleMixin ::: Dumping chicken registry! (in init)");
+                for (ChickensRegistryItem chicken : ChickensRegistry.getItems()) {
+                    CraftTweakerAPI.logInfo("UTChickenModuleMixin ::: " + chicken.getRegistryName().toString());
+                }
+            }
+            ResourceLocation registryName = new ResourceLocation(ContentTweaker.MOD_ID, chickenRepresentation.name);
+            ChickensRegistryItem item = ChickensRegistry.getByRegistryName(registryName.toString());
+            if (item == null) {
+                CraftTweakerAPI.logError("Failed to find chicken item " + registryName + " in registry");
+                return;
+            }
+
+            ChickensRegistryItem parentOneItem = chickenRepresentation.parentOne != null ?
+                    ChickensRegistry.getByResourceLocation(chickenRepresentation.parentOne.getInternal()) : null;
+            ChickensRegistryItem parentTwoItem = chickenRepresentation.parentTwo != null ?
+                    ChickensRegistry.getByResourceLocation(chickenRepresentation.parentTwo.getInternal()) : null;
+            if (parentOneItem != null && parentTwoItem != null) {
+                item.setParentsNew(parentOneItem, parentTwoItem);
+            } else {
+                item.setNoParents();
+            }
+
+            if (chickenRepresentation.layItem != null) {
+                item.setLayItem(CraftTweakerMC.getItemStack(chickenRepresentation.layItem));
+            }
+
+            if (chickenRepresentation.dropItem != null) {
+                item.setDropItem(CraftTweakerMC.getItemStack(chickenRepresentation.dropItem));
+            }
+
+            if (chickenRepresentation.spawnType != null) {
+                SpawnType actualSpawnType = null;
+                for (SpawnType spawnTypeEnum : SpawnType.values()) {
+                    if (spawnTypeEnum.toString().equalsIgnoreCase(chickenRepresentation.spawnType)) {
+                        actualSpawnType = spawnTypeEnum;
+                    }
+                }
+                if (actualSpawnType != null) {
+                    item.setSpawnType(actualSpawnType);
+                } else {
+                    CraftTweakerAPI.logError("Failed to find SpawnType for String: " + chickenRepresentation.spawnType);
+                }
+            }
+
+            item.setLayCoefficient(chickenRepresentation.layCoefficient);
+        }
+    }
+}
