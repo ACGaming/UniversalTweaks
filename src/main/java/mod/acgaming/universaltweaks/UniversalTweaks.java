74754c20dfb52d7c75b0ff00b292c816df2fba98
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-03-22 16:02:47 +0100

    Bump version

--- src/main/java/mod/acgaming/universaltweaks/UniversalTweaks.java
+++ src/main/java/mod/acgaming/universaltweaks/UniversalTweaks.java
@@ -35,7 +35,7 @@ public class UniversalTweaks
 {
     public static final String MODID = "universaltweaks";
     public static final String NAME = "Universal Tweaks";
-    public static final String VERSION = "1.12.2-1.4.2";
+    public static final String VERSION = "1.12.2-1.5.0";
     public static final String DEPENDENCIES = "required-after:mixinbooter;after:biomesoplenty;after:botania;after:contenttweaker;after:customspawner;after:epicsiegemod;after:extratrees;after:forestry;after:roost;after:storagedrawers;after:tconstruct;after:thaumcraft";
     public static final Logger LOGGER = LogManager.getLogger(NAME);
 
