e8eaa414aad29003091c3d814a56345ae75036bd
UeberallGebannt <paulmertens63@gmail.com>
2023-03-22 12:12:26 +0100

    Add chance for charged creepers to spawn naturally

---
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/UTChargedCreepersSpawnChance.java
@@ -0,0 +1,49 @@
+package mod.acgaming.universaltweaks.tweaks;
+
+import mod.acgaming.universaltweaks.UniversalTweaks;
+import mod.acgaming.universaltweaks.config.UTConfig;
+import mod.acgaming.universaltweaks.util.UTRandomUtil;
+import net.minecraft.entity.effect.EntityLightningBolt;
+import net.minecraft.entity.monster.EntityCreeper;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+
+@Mod.EventBusSubscriber(modid = UniversalTweaks.MODID)
+public class UTChargedCreepersSpawnChance
+{
+
+    // All creepers with this tag will be ignored and not checked again
+    private static final String SPAWNED_CHECK_TAG = "utChargedCreepersSpawnChance.spawned";
+
+    // Also ignore all creepers modified by NCC (compatible with old worlds)
+    private static final String NCC_CHECK_TAG = "naturallychargedcreepers.checked";
+
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public static void utChargedCreepersSpawnChance(EntityJoinWorldEvent event)
+    {
+        if (!(event.getEntity() instanceof EntityCreeper)) return;
+
+        if (UTConfig.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTChargedCreepersSpawnChance ::: Creeper Spawn Event");
+
+        double chargedChance = UTConfig.TWEAKS_ENTITIES.utChargedCreeperSpawnChance;
+        if (chargedChance <= 0.0d) return;
+
+        if (event.getEntity().getTags().contains(SPAWNED_CHECK_TAG) || event.getEntity().getTags().contains(NCC_CHECK_TAG)) return;
+        event.getEntity().addTag(SPAWNED_CHECK_TAG);
+
+        if (!UTRandomUtil.chance(chargedChance)) return;
+
+        if (UTConfig.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTChargedCreepersSpawnChance ::: Creeper Spawn Event Charged");
+
+        EntityCreeper creeper = (EntityCreeper) event.getEntity();
+        BlockPos pos = creeper.getPosition();
+
+        creeper.onStruckByLightning(new EntityLightningBolt(event.getWorld(), pos.getX(), pos.getY(), pos.getZ(), true));
+        creeper.extinguish();
+
+    }
+}
