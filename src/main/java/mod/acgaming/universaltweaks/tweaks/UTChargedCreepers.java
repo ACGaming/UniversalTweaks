db5b86e34f4c08241cf008d7d4cc64917a1b6301
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-03-28 15:21:23 +0200

    Side check for charged creepers tweak

--- src/main/java/mod/acgaming/universaltweaks/tweaks/UTChargedCreepers.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/UTChargedCreepers.java
@@ -17,15 +17,15 @@ import mod.acgaming.universaltweaks.util.UTRandomUtil;
 public class UTChargedCreepers
 {
     // All creepers with this tag will be ignored and not checked again
-    private static final String SPAWNED_CHECK_TAG = "utChargedCreepers.spawned";
+    public static final String SPAWNED_CHECK_TAG = "utChargedCreepers.spawned";
 
     // Also ignore all creepers modified by NCC (compatible with old worlds)
-    private static final String NCC_CHECK_TAG = "naturallychargedcreepers.checked";
+    public static final String NCC_CHECK_TAG = "naturallychargedcreepers.checked";
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void utChargedCreepersSpawnChance(EntityJoinWorldEvent event)
     {
-        if (!(event.getEntity() instanceof EntityCreeper)) return;
+        if (!(event.getEntity() instanceof EntityCreeper) || event.getWorld().isRemote) return;
 
         if (UTConfig.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTChargedCreepers ::: Creeper Spawn Event");
 
