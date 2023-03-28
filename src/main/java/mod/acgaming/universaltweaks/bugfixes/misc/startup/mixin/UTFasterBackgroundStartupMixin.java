400254cf46f0d9774b35f60d5f7afff4e5aad984
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-03-28 16:39:22 +0200

    Simplify tooltip check, closes #100

--- src/main/java/mod/acgaming/universaltweaks/bugfixes/misc/startup/mixin/UTFasterBackgroundStartupMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/bugfixes/misc/startup/mixin/UTFasterBackgroundStartupMixin.java
@@ -5,19 +5,15 @@ import java.util.List;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.ForgeEventFactory;
-import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.client.FMLClientHandler;
 
 import mod.acgaming.universaltweaks.config.UTConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import zone.rong.mixinextras.injector.WrapWithCondition;
 
 // Courtesy of EverNife
-@Mixin(value = ForgeEventFactory.class, remap = false)
+@Mixin(ItemStack.class)
 public class UTFasterBackgroundStartupMixin
 {
     /**
@@ -28,13 +24,9 @@ public class UTFasterBackgroundStartupMixin
      * if the user is pressing a Keyboard button to show some info, but, as you are on alt-tab
      * this process can take a looong time (a modpack startup from 3 min can go up to 9 min)
      */
-    @Inject(method = "onItemTooltip", at = @At(value = "HEAD"), cancellable = true)
-    private static void utFasterBackgroundStartup(ItemStack itemStack, EntityPlayer entityPlayer, List<String> toolTip, ITooltipFlag flags, CallbackInfoReturnable<ItemTooltipEvent> cir)
+    @WrapWithCondition(method = "getTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/event/ForgeEventFactory;onItemTooltip(Lnet/minecraft/item/ItemStack;Lnet/minecraft/entity/player/EntityPlayer;Ljava/util/List;Lnet/minecraft/client/util/ITooltipFlag;)Lnet/minecraftforge/event/entity/player/ItemTooltipEvent;"), remap = false)
+    public boolean utFasterBackgroundStartup(ItemStack itemStack, EntityPlayer entityPlayer, List<String> toolTip, ITooltipFlag flags)
     {
-        if (!UTConfig.BUGFIXES_MISC.utFasterBackgroundStartupToggle) return;
-        //if (UTConfig.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTFasterBackgroundStartup ::: On item tooltip");
-        ItemTooltipEvent event = new ItemTooltipEvent(itemStack, entityPlayer, toolTip, flags);
-        if (!FMLClientHandler.instance().isLoading()) MinecraftForge.EVENT_BUS.post(event);
-        cir.setReturnValue(event);
+        return !UTConfig.BUGFIXES_MISC.utFasterBackgroundStartupToggle || !FMLClientHandler.instance().isLoading();
     }
 }
