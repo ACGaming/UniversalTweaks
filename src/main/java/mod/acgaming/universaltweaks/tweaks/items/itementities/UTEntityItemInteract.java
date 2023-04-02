75c1ff24390f14e89bc0b797e21831aaa9d6e6da
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-04-02 16:53:58 +0200

    Streamline entity ray tracing, item entity pickup

--- src/main/java/mod/acgaming/universaltweaks/tweaks/items/itementities/UTEntityItemInteract.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/items/itementities/UTEntityItemInteract.java
@@ -1,8 +1,9 @@
-package mod.acgaming.universaltweaks.tweaks.misc.swingthroughgrass;
+package mod.acgaming.universaltweaks.tweaks.items.itementities;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -13,24 +14,22 @@ import mod.acgaming.universaltweaks.UniversalTweaks;
 import mod.acgaming.universaltweaks.config.UTConfig;
 import mod.acgaming.universaltweaks.util.UTRayTraceEntity;
 
-// Courtesy of Exidex, Rongmario
 @Mod.EventBusSubscriber(modid = UniversalTweaks.MODID)
-public class UTSwingThroughGrass
+public class UTEntityItemInteract
 {
     @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public static void utSwingThroughGrass(PlayerInteractEvent.LeftClickBlock event)
+    public static void utEntityItemInteract(PlayerInteractEvent.RightClickBlock event)
     {
-        if (!UTConfig.TWEAKS_MISC.SWING_THROUGH_GRASS.utSwingThroughGrassToggle) return;
-        if (UTConfig.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTSwingThroughGrass ::: Left click block event");
+        if (UTConfig.TWEAKS_ITEMS.ITEM_ENTITIES.utIEAutomaticPickupToggle) return;
+        if (UTConfig.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTEntityItemInteract ::: Right click block");
         IBlockState state = event.getWorld().getBlockState(event.getPos()).getActualState(event.getWorld(), event.getPos());
-        Block block = state.getBlock();
-        if ((UTSwingThroughGrassLists.blacklistedBlocks.contains(block) || state.getCollisionBoundingBox(event.getWorld(), event.getPos()) != Block.NULL_AABB) && !UTSwingThroughGrassLists.whitelistedBlocks.contains(block)) return;
+        if (state.getCollisionBoundingBox(event.getWorld(), event.getPos()) != Block.NULL_AABB) return;
         EntityPlayer player = event.getEntityPlayer();
         if (player == null) return;
         Entity entity = UTRayTraceEntity.rayTraceEntityAsPlayer(player);
-        if (entity != null)
+        if (entity instanceof EntityItem)
         {
-            player.attackTargetEntityWithCurrentItem(entity);
+            entity.processInitialInteract(player, player.getActiveHand());
             event.setCanceled(true);
         }
     }
