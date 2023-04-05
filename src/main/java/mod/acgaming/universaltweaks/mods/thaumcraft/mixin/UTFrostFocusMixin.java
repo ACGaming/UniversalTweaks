6fa8d61c9429a6b12a6024caab5ba4dfe424d277
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-04-05 19:48:16 +0200

    Pull request follow-up

--- src/main/java/mod/acgaming/universaltweaks/mods/thaumcraft/mixin/UTFrostFocusMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/mods/thaumcraft/mixin/UTFrostFocusMixin.java
@@ -1,11 +1,9 @@
 package mod.acgaming.universaltweaks.mods.thaumcraft.mixin;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.util.SoundCategory;
 
-import mod.acgaming.universaltweaks.UniversalTweaks;
 import mod.acgaming.universaltweaks.config.UTConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import thaumcraft.api.casters.FocusEffect;
@@ -16,12 +14,10 @@ import thaumcraft.common.lib.SoundsTC;
 @Mixin(FocusEffectFrost.class)
 public abstract class UTFrostFocusMixin extends FocusEffect
 {
-	@Override
-	public void onCast(final Entity caster)
-	{
-        if (UTConfig.MOD_INTEGRATION.THAUMCRAFT.utTCFrostFocusSoundRevampToggle) caster.world.playSound((EntityPlayer)null, caster.getPosition().up(), SoundsTC.ice, SoundCategory.PLAYERS, 0.6F, 1.0F + 
-		(float)(caster.world.rand.nextGaussian() * 0.05F));
-        else caster.world.playSound((EntityPlayer)null, caster.getPosition().up(), SoundEvents.ENTITY_ZOMBIE_VILLAGER_CURE, SoundCategory.PLAYERS, 0.2F, 1.0F + 
-		(float)(caster.world.rand.nextGaussian() * 0.05F));
+    @Override
+    public void onCast(final Entity caster)
+    {
+        if (UTConfig.MOD_INTEGRATION.THAUMCRAFT.utTCFrostFocusSoundRevampToggle) caster.world.playSound(null, caster.getPosition().up(), SoundsTC.ice, SoundCategory.PLAYERS, 0.6F, 1.0F + (float) (caster.world.rand.nextGaussian() * 0.05F));
+        else caster.world.playSound(null, caster.getPosition().up(), SoundEvents.ENTITY_ZOMBIE_VILLAGER_CURE, SoundCategory.PLAYERS, 0.2F, 1.0F + (float) (caster.world.rand.nextGaussian() * 0.05F));
     }
 }
--- src/main/java/mod/acgaming/universaltweaks/mods/thaumcraft/mixin/UTMediumBoltFocusMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/mods/thaumcraft/mixin/UTMediumBoltFocusMixin.java
@@ -1,10 +1,6 @@
 package mod.acgaming.universaltweaks.mods.thaumcraft.mixin;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.SoundCategory;
-import net.minecraft.util.math.RayTraceResult;
-import net.minecraft.util.math.Vec3d;
 
 import mod.acgaming.universaltweaks.UniversalTweaks;
 import mod.acgaming.universaltweaks.config.UTConfig;
@@ -16,24 +12,16 @@ import thaumcraft.api.casters.Trajectory;
 import thaumcraft.common.items.casters.foci.FocusMediumBolt;
 import thaumcraft.common.items.casters.foci.FocusMediumTouch;
 import thaumcraft.common.lib.SoundsTC;
-import thaumcraft.common.lib.utils.EntityUtils;
 
 // Courtesy of Turkey9002
 @Mixin(FocusMediumBolt.class)
 public class UTMediumBoltFocusMixin extends FocusMediumTouch
 {
-    @Inject(method = "execute", at = @At(value = "HEAD"), cancellable = true, remap = false)
-	public void utMediumBoltFocusSound(final Trajectory trajectory, CallbackInfoReturnable<Boolean> cir)
-	{
+    @Inject(method = "execute", at = @At(value = "RETURN"), remap = false)
+    public void utMediumBoltFocusSound(Trajectory trajectory, CallbackInfoReturnable<Boolean> cir)
+    {
         if (!UTConfig.MOD_INTEGRATION.THAUMCRAFT.utTCBoltMediumSoundToggle) return;
         if (UTConfig.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTMediumBoltFocus ::: Execute");
-		final float range = 16.0F;
-        Vec3d end = trajectory.direction.normalize();
-		RayTraceResult ray = EntityUtils.getPointedEntityRay(this.getPackage().world, (Entity)this.getPackage().getCaster(), trajectory.source, end, 0.25F, range, 0.25F, false);
-		if (ray == null || ray != null) 
-		{
-			this.getPackage().world.playSound((EntityPlayer)null, this.getPackage().getCaster().getPosition().up(), SoundsTC.shock, SoundCategory.PLAYERS, 0.175F, 1.0F + 
-	        		(float)(this.getPackage().getCaster().world.rand.nextGaussian() * 0.05F));
-        }
+        this.getPackage().world.playSound(null, this.getPackage().getCaster().getPosition().up(), SoundsTC.shock, SoundCategory.PLAYERS, 0.175F, 1.0F + (float) (this.getPackage().getCaster().world.rand.nextGaussian() * 0.05F));
     }
 }
