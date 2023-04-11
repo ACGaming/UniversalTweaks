a66e3410367786f56265f1e4c0c67fa6ed9576e9
ChaosStrikez <jjchung276@gmail.com>
2023-04-11 01:02:55 -0700

    Remove empty space

--- src/main/java/mod/acgaming/universaltweaks/tweaks/misc/commands/seed/mixin/UTCopySeedMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/misc/commands/seed/mixin/UTCopySeedMixin.java
@@ -8,7 +8,6 @@ import net.minecraft.util.text.event.ClickEvent;
 
 import mod.acgaming.universaltweaks.UniversalTweaks;
 import mod.acgaming.universaltweaks.config.UTConfig;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
