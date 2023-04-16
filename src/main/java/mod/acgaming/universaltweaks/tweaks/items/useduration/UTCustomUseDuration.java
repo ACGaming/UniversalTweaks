ffe706e3ad437a4349176e27c724f60f9f8be134
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-04-16 10:47:04 +0200

    Switch custom use duration tweak to event

--- src/main/java/mod/acgaming/universaltweaks/tweaks/items/useduration/UTCustomUseDuration.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/items/useduration/UTCustomUseDuration.java
@@ -2,17 +2,26 @@ package mod.acgaming.universaltweaks.tweaks.items.useduration;
 
 import java.util.Map;
 
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import mod.acgaming.universaltweaks.UniversalTweaks;
 import mod.acgaming.universaltweaks.config.UTConfig;
 
+@Mod.EventBusSubscriber(modid = UniversalTweaks.MODID)
 public class UTCustomUseDuration
 {
     public static Map<String, Integer> itemUseDurationMap = new Object2IntOpenHashMap<>();
     public static Map<String, Integer> itemUseCooldownMap = new Object2IntOpenHashMap<>();
+    public static int used = 0;
 
     public static void initItemUseMaps()
     {
@@ -42,5 +51,50 @@ public class UTCustomUseDuration
             e.printStackTrace();
         }
         UniversalTweaks.LOGGER.info("Item use duration map initialized");
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public static void utCustomUseDurationTick(LivingEntityUseItemEvent.Tick event)
+    {
+        if (event.getEntityLiving().getEntityWorld().isRemote) return;
+        ItemStack stack = event.getItem();
+        int duration = getCustomDuration(stack);
+        if (duration < 0) return;
+        used++;
+        if (used >= duration)
+        {
+            used = 0;
+            EntityLivingBase entity = event.getEntityLiving();
+            entity.stopActiveHand();
+            int cooldown = getCustomCooldown(stack);
+            if (cooldown > 0 && entity instanceof EntityPlayer) ((EntityPlayer) entity).getCooldownTracker().setCooldown(stack.getItem(), cooldown);
+            event.setCanceled(true);
+        }
+    }
+
+    public static int getCustomDuration(ItemStack stack)
+    {
+        int meta = 0;
+        try
+        {
+            if (stack.getHasSubtypes()) meta = stack.getMetadata();
+        }
+        catch (Exception ignored) {}
+        String itemKey = stack.getItem().toString() + meta;
+        if (UTCustomUseDuration.itemUseDurationMap.containsKey(itemKey)) return UTCustomUseDuration.itemUseDurationMap.get(itemKey);
+        return -1;
+    }
+
+    public static int getCustomCooldown(ItemStack stack)
+    {
+        int meta = 0;
+        try
+        {
+            if (stack.getHasSubtypes()) meta = stack.getMetadata();
+        }
+        catch (Exception ignored) {}
+        String itemKey = stack.getItem().toString() + meta;
+        if (UTCustomUseDuration.itemUseCooldownMap.containsKey(itemKey)) return UTCustomUseDuration.itemUseCooldownMap.get(itemKey);
+        return -1;
     }
 }
