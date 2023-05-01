e6258bbf5c3b2c85e7f49c5cc490ddcb98161308
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-05-01 13:22:35 +0200

    Send server difficulty packet on dimension change
    LoliKingdom/LoliASM#128

---
+++ src/main/java/mod/acgaming/universaltweaks/bugfixes/entities/dimensionchange/mixin/UTDimensionChangePlayerListMixin.java
@@ -0,0 +1,30 @@
+package mod.acgaming.universaltweaks.bugfixes.entities.dimensionchange.mixin;
+
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.network.play.server.SPacketServerDifficulty;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.management.PlayerList;
+import net.minecraftforge.common.util.ITeleporter;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+// MC-44471
+// https://bugs.mojang.com/browse/MC-44471
+@Mixin(PlayerList.class)
+public class UTDimensionChangePlayerListMixin
+{
+    @Shadow
+    @Final
+    private MinecraftServer server;
+
+    @Inject(method = "transferPlayerToDimension(Lnet/minecraft/entity/player/EntityPlayerMP;ILnet/minecraftforge/common/util/ITeleporter;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/management/PlayerList;updatePermissionLevel(Lnet/minecraft/entity/player/EntityPlayerMP;)V"))
+    public void utTransferPlayerToDimension(EntityPlayerMP player, int dimensionIn, ITeleporter teleporter, CallbackInfo ci)
+    {
+        player.connection.sendPacket(new SPacketServerDifficulty(this.server.getWorld(player.dimension).getWorldInfo().getDifficulty(), this.server.getWorld(player.dimension).getWorldInfo().isDifficultyLocked()));
+    }
+}
