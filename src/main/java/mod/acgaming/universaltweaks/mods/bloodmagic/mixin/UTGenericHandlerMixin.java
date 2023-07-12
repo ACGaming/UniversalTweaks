5fc5cbc74eb833147d1e63bb1e90ec2733ed3d9c
ChaosStrikez <jjchung276@gmail.com>
2023-07-11 17:35:20 -0700

    Optimize hellfire forge, add memory leak fix

---
+++ src/main/java/mod/acgaming/universaltweaks/mods/bloodmagic/mixin/UTGenericHandlerMixin.java
@@ -0,0 +1,35 @@
+package mod.acgaming.universaltweaks.mods.bloodmagic.mixin;
+
+import java.util.Map;
+
+import net.minecraft.world.World;
+import net.minecraftforge.event.world.WorldEvent;
+
+import WayofTime.bloodmagic.ritual.AreaDescriptor;
+import WayofTime.bloodmagic.ritual.IMasterRitualStone;
+import WayofTime.bloodmagic.util.handler.event.GenericHandler;
+import mod.acgaming.universaltweaks.config.UTConfig;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+// Courtesy of michaelPoul
+@Mixin(value = GenericHandler.class, remap = false)
+public class UTGenericHandlerMixin
+{
+    @Shadow
+    public static Map<World, Map<IMasterRitualStone, AreaDescriptor>> forceSpawnMap;
+    @Shadow
+    public static Map<World, Map<IMasterRitualStone, AreaDescriptor>> preventSpawnMap;
+
+    @Inject(method = "onWorldUnload", at = @At(value = "TAIL"), locals = LocalCapture.CAPTURE_FAILHARD)
+    private static void utRemoveRemainingWorld(WorldEvent.Unload event, CallbackInfo ci, World world)
+    {
+        if (!UTConfig.MOD_INTEGRATION.BLOOD_MAGIC.utBMWorldUnloadToggle) return;
+        forceSpawnMap.remove(world);
+        preventSpawnMap.remove(world);
+    }
+}
