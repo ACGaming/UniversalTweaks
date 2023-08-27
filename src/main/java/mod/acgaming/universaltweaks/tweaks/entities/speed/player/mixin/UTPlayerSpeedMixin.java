1892cf1e2d18bcdf0d4001dfbf55fcba0d4c8d49
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-08-27 08:56:44 +0200

    Adjust player speed tweak hooks, closes #247

--- src/main/java/mod/acgaming/universaltweaks/tweaks/entities/speed/player/mixin/UTPlayerSpeedMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/entities/speed/player/mixin/UTPlayerSpeedMixin.java
@@ -5,20 +5,21 @@ import net.minecraft.entity.player.PlayerCapabilities;
 import mod.acgaming.universaltweaks.config.UTConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerCapabilities.class)
 public class UTPlayerSpeedMixin
 {
-    @Redirect(method = "getFlySpeed", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerCapabilities;flySpeed:F"))
-    public float utModifyFlySpeed(PlayerCapabilities instance)
+    @Inject(method = "getFlySpeed", at = @At(value = "RETURN"), cancellable = true)
+    public void utModifyFlySpeed(CallbackInfoReturnable<Float> cir)
     {
-        return (float) UTConfig.TWEAKS_ENTITIES.PLAYER_SPEED.utPlayerFlySpeed;
+        if (UTConfig.TWEAKS_ENTITIES.PLAYER_SPEED.utPlayerSpeedToggle) cir.setReturnValue((float) UTConfig.TWEAKS_ENTITIES.PLAYER_SPEED.utPlayerFlySpeed);
     }
 
-    @Redirect(method = "getWalkSpeed", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerCapabilities;walkSpeed:F"))
-    public float utModifyWalkSpeed(PlayerCapabilities instance)
+    @Inject(method = "getWalkSpeed", at = @At(value = "RETURN"), cancellable = true)
+    public void utModifyWalkSpeed(CallbackInfoReturnable<Float> cir)
     {
-        return (float) UTConfig.TWEAKS_ENTITIES.PLAYER_SPEED.utPlayerWalkSpeed;
+        if (UTConfig.TWEAKS_ENTITIES.PLAYER_SPEED.utPlayerSpeedToggle) cir.setReturnValue((float) UTConfig.TWEAKS_ENTITIES.PLAYER_SPEED.utPlayerWalkSpeed);
     }
 }
