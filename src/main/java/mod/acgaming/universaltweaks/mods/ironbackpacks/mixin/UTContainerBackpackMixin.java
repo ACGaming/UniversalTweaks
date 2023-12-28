63fed4c2cc766d9b65788733911301f7382c20d9
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-12-28 15:27:24 +0100

    Disable Iron Backpacks container dupe fix, closes #299
    TODO: Find better solution

--- src/main/java/mod/acgaming/universaltweaks/mods/ironbackpacks/mixin/UTContainerBackpackMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/mods/ironbackpacks/mixin/UTContainerBackpackMixin.java
@@ -26,6 +26,7 @@ public class UTContainerBackpackMixin extends Container
     @ParametersAreNonnullByDefault
     public boolean canInteractWith(EntityPlayer playerIn)
     {
+        // Currently breaks hotkey functionality
         return this.backpackStack.getItem() instanceof IBackpack && (playerIn.getHeldItemMainhand() == this.backpackStack || playerIn.getHeldItemOffhand() == this.backpackStack);
     }
 }
