86ac2ed8ff69bf106f53c475c46a5513b84817e2
Me <135455255+IcarussOne@users.noreply.github.com>
2024-02-05 11:19:59 -0600

    Disable Advancements Tweak

---
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/misc/advancements/mixin/UTAdvancementManagerMixin.java
@@ -0,0 +1,19 @@
+package mod.acgaming.universaltweaks.tweaks.misc.advancements.mixin;
+
+import mod.acgaming.universaltweaks.config.UTConfigTweaks;
+import net.minecraft.advancements.AdvancementManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+// Courtesy of fonnymunkey
+@Mixin(AdvancementManager.class)
+public abstract class UTAdvancementManagerMixin
+{
+    @Inject(method = "reload", at = @At("HEAD"), cancellable = true)
+    public void utAdvancementManagerReload(CallbackInfo ci)
+    {
+        if (UTConfigTweaks.MISC.utDisableAdvancementsToggle) ci.cancel();
+    }
+}
