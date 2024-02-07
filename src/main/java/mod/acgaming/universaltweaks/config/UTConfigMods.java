fc886cb5edf7915f426a8dddbb8888b0ad8e370a
jchung01 <jjchung276@gmail.com>
2024-02-06 20:17:47 -0800

    Update mod section of README

--- src/main/java/mod/acgaming/universaltweaks/config/UTConfigMods.java
+++ src/main/java/mod/acgaming/universaltweaks/config/UTConfigMods.java
@@ -203,7 +203,7 @@ public class UTConfigMods
     {
         @Config.RequiresMcRestart
         @Config.Name("Optimized Item Transport")
-        @Config.Comment("Makes an optimization to reduce tick overhead of AbyssalCraft's item transport system")
+        @Config.Comment("Optimizes AbyssalCraft's item transport system to reduce tick overhead")
         public boolean utOptimizedItemTransferToggle = true;
     }
 
@@ -227,7 +227,7 @@ public class UTConfigMods
     {
         @Config.RequiresMcRestart
         @Config.Name("Inventory-less GUI Compatibility")
-        @Config.Comment("Fixes AoA player ticking in certain GUIs without player inventories (i.e. Flux Networks GUI)")
+        @Config.Comment("Fixes AoA player ticking in certain GUIs without player inventories (i.e. Flux Networks, Nuclearcraft)")
         public boolean utFixPlayerTickInInventorylessGui = false;
     }
 
@@ -310,7 +310,7 @@ public class UTConfigMods
     {
         @Config.RequiresMcRestart
         @Config.Name("Invisible Wall Render Fix")
-        @Config.Comment("Fixes some compact machine walls being invisible if Nothirium 0.2.x+ or Vintagium is installed")
+        @Config.Comment("Fixes some compact machine walls being invisible if Nothirium 0.2.x (and up) or Vintagium is installed")
         public boolean utCMRenderFixToggle = true;
     }
 
@@ -506,7 +506,7 @@ public class UTConfigMods
         @Config.Name("Radiation Environment Map")
         @Config.Comment
             ({
-                "Changes the data table of the radiation environment handler to improve performance",
+                "Changes the data table of the radiation environment handler to improve tick time",
                 "CONCURRENT_HASHMAP:        NuclearCraft default",
                 "CONCURRENT_LINKED_HASHMAP: Keeps order of radiation environment info to improve iteration - Better performance",
                 "CONCURRENT_LINKED_QUEUE:   Uses a queue to avoid iteration - Best performance"
@@ -543,7 +543,7 @@ public class UTConfigMods
         @Config.Name("ContentTweaker: Early Register CT Chickens")
         @Config.Comment
             ({
-                "Improves load time by registering CT chickens early for Roost to detect them",
+                "Improves load time by registering ContentTweaker chickens early for Roost to detect them",
                 "Note: If you would like to use ContentTweaker's MaterialSystem Parts for the layed item, in your script you must:",
                 "1) Use '#loader finalize_contenttweaker', not '#loader contenttweaker'",
                 "2) Use the Material Part Bracket Handler to reference the item"
