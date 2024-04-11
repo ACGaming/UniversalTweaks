7a37e5cc7e0a872827818106aa44f10ae13f6716
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2024-04-11 01:35:51 -0700

    change to ModifyExpressionValue

--- src/main/java/mod/acgaming/universaltweaks/mods/extrautilities/breakcreativemill/mixin/UTBreakableCreativeMill.java
+++ src/main/java/mod/acgaming/universaltweaks/mods/extrautilities/breakcreativemill/mixin/UTBreakableCreativeMill.java
@@ -1,25 +1,21 @@
 package mod.acgaming.universaltweaks.mods.extrautilities.breakcreativemill.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.rwtema.extrautils2.ExtraUtils2;
 import com.rwtema.extrautils2.backend.XUBlockStatic;
 import com.rwtema.extrautils2.blocks.BlockPassiveGenerator;
 import mod.acgaming.universaltweaks.config.UTConfigMods;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 // Courtesy of WaitingIdly
 @Mixin(value = BlockPassiveGenerator.class, remap = false)
 public abstract class UTBreakableCreativeMill extends XUBlockStatic
 {
-    @Inject(method = "getBlockHardness", at = @At(value = "HEAD"), cancellable = true)
-    private void utBreakCreativeMill(IBlockState blockState, World worldIn, BlockPos pos, CallbackInfoReturnable<Float> cir)
+    @ModifyExpressionValue(method = "getBlockHardness", at = @At(value = "FIELD", target = "Lcom/rwtema/extrautils2/ExtraUtils2;allowCreativeBlocksToBeBroken:Z"))
+    private boolean utBreakCreativeMill(boolean original)
     {
-        if (!UTConfigMods.EXTRA_UTILITIES.utFixCreativeMillHarvestability) return;
-        if (ExtraUtils2.allowNonCreativeHarvest) cir.setReturnValue(super.getBlockHardness(blockState, worldIn, pos));
+        if (!UTConfigMods.EXTRA_UTILITIES.utFixCreativeMillHarvestability) return original;
+        return ExtraUtils2.allowNonCreativeHarvest;
     }
 }
