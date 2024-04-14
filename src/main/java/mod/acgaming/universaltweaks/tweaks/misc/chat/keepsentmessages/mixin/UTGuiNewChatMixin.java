f8da661dd2102bff726588985f21bd715886bfa9
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2024-04-14 07:52:28 -0700

    add Courtesy of WaitingIdly

--- src/main/java/mod/acgaming/universaltweaks/tweaks/misc/chat/keepsentmessages/mixin/UTGuiNewChatMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/misc/chat/keepsentmessages/mixin/UTGuiNewChatMixin.java
@@ -7,6 +7,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+// Courtesy of WaitingIdly
 @Mixin(value = GuiNewChat.class)
 public class UTGuiNewChatMixin
 {
--- src/main/java/mod/acgaming/universaltweaks/tweaks/misc/chat/maximumlines/mixin/UTGuiNewChatMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/misc/chat/maximumlines/mixin/UTGuiNewChatMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
+// Courtesy of WaitingIdly
 @Mixin(value = GuiNewChat.class)
 public class UTGuiNewChatMixin
 {
