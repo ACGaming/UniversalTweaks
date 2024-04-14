d88a1d54e329f44b0f844ff109e312c2d66a64a8
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2024-04-14 05:23:01 -0700

    block specific toasts

---
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/misc/toastcontrol/mixin/UTGuiToastMixin.java
@@ -0,0 +1,24 @@
+package mod.acgaming.universaltweaks.tweaks.misc.toastcontrol.mixin;
+
+import mod.acgaming.universaltweaks.UniversalTweaks;
+import mod.acgaming.universaltweaks.config.UTConfigTweaks;
+import net.minecraft.client.gui.toasts.GuiToast;
+import net.minecraft.client.gui.toasts.IToast;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Arrays;
+
+@Mixin(value = GuiToast.class)
+public abstract class UTGuiToastMixin
+{
+    @Inject(method = "add", at = @At(value = "HEAD"), cancellable = true)
+    private void utFilterToasts(IToast toastIn, CallbackInfo ci)
+    {
+        if (UTConfigTweaks.MISC.TOAST_CONTROL.utToastNameLogging) UniversalTweaks.LOGGER.info("UTGuiToastMixin ::: Displaying Toast: " + toastIn.getClass().getName());
+        boolean isWhitelist = UTConfigTweaks.MISC.TOAST_CONTROL.utToastControlClassListMode == UTConfigTweaks.EnumLists.WHITELIST;
+        if (Arrays.asList(UTConfigTweaks.MISC.TOAST_CONTROL.utToastControlClassList).contains(toastIn.getClass().getName()) != isWhitelist) ci.cancel();
+    }
+}
