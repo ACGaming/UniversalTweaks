6e6b1c60e9592fe7d97019b228300e087290fc7c
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2024-04-17 07:45:09 -0700

    optimize cabbage imports

--- src/main/java/mod/acgaming/universaltweaks/mods/erebus/cabbage/mixin/UTCabbageMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/mods/erebus/cabbage/mixin/UTCabbageMixin.java
@@ -7,21 +7,17 @@ import erebus.items.ItemErebusFood;
 import mod.acgaming.universaltweaks.config.UTConfigMods;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import javax.annotation.Nonnull;
 import java.util.List;
-import java.util.Random;
 
 // Courtesy of WaitingIdly
 @Mixin(value = BlockCabbage.class, remap = false)
