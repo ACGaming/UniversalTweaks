78e0577ed8028fcf902ee4ef3edd299741ca8cca
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2024-04-17 16:22:50 -0700

    use WrapOperation

--- src/main/java/mod/acgaming/universaltweaks/mods/astralsorcery/mixin/UTResearchManagerMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/mods/astralsorcery/mixin/UTResearchManagerMixin.java
@@ -1,20 +1,21 @@
 package mod.acgaming.universaltweaks.mods.astralsorcery.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import hellfirepvp.astralsorcery.common.data.research.ResearchManager;
 import mod.acgaming.universaltweaks.config.UTConfigMods;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 // Courtesy of WaitingIdly
 @Mixin(value = ResearchManager.class, remap = false)
 public abstract class UTResearchManagerMixin
 {
-    @Redirect(method = {"informCraftingInfusionCompletion", "informCraftingAltarCompletion"}, at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;)V"))
-    private static void utInformCraftingInfusionCompletion(Logger logger, String log)
+    @WrapOperation(method = {"informCraftingInfusionCompletion", "informCraftingAltarCompletion"}, at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;)V"))
+    private static void utInformCraftingInfusionCompletion(Logger logger, String log, Operation<Void> original)
     {
-        if (!UTConfigMods.ASTRAL_SORCERY.utMissingPlayerLogLevelDowngrade) return;
-        logger.debug(log);
+        if (!UTConfigMods.ASTRAL_SORCERY.utMissingPlayerLogLevelDowngrade) original.call(logger, log);
+        else logger.debug(log);
     }
 }
