1eea103d198865008392327cf763e3e0817f6f43
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2024-04-17 08:26:58 -0700

    increase EXTRA_WIDTH and mark unique

--- src/main/java/mod/acgaming/universaltweaks/tweaks/misc/gui/ping/mixin/UTGuiPlayerTabOverlayMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/misc/gui/ping/mixin/UTGuiPlayerTabOverlayMixin.java
@@ -8,14 +8,19 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = GuiPlayerTabOverlay.class)
 public abstract class UTGuiPlayerTabOverlayMixin
 {
+    @Unique
     private static final int PING_TEXT_RENDER_OFFSET = 13;
-    private static final int EXTRA_WIDTH = 33;
+    @Unique
+    private static final int EXTRA_WIDTH = 37;
 
     @Shadow
     @Final
