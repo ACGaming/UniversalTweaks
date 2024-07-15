445b52be737306cdc180b78d85861392bd976ced
jchung01 <jjchung276@gmail.com>
2024-07-14 20:06:03 -0700

    Simplify particle spawning bugfix

--- src/main/java/mod/acgaming/universaltweaks/bugfixes/misc/particlespawning/mixin/UTParticleSpawningMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/bugfixes/misc/particlespawning/mixin/UTParticleSpawningMixin.java
@@ -1,15 +1,14 @@
 package mod.acgaming.universaltweaks.bugfixes.misc.particlespawning.mixin;
 
 import java.util.Objects;
-
+import mod.acgaming.universaltweaks.config.UTConfigBugfixes;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.world.ServerWorldEventHandler;
-
-import mod.acgaming.universaltweaks.config.UTConfigBugfixes;
+import net.minecraft.world.WorldServer;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 
 // MC-10369, MC-93826
 // https://bugs.mojang.com/browse/MC-10369
@@ -18,16 +17,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerWorldEventHandler.class)
 public abstract class UTParticleSpawningMixin
 {
+    @Final
+    @Shadow
+    private WorldServer world;
+
     /**
-     * Fixes particles not properly being spawned on the client
+     * @author IcarussOne
+     * @reason Fixes particles not properly being spawned on the client
      * Based on a patch by RandomPatches
      * https://github.com/TheRandomLabs/RandomPatches/blob/1.12/src/main/java/com/therandomlabs/randompatches/patch/ServerWorldEventHandlerPatch.java
      */
-    @Inject(method = "spawnParticle(IZDDDDDD[I)V", at = @At("HEAD"))
-    public void utSpawnParticle(int particleID, boolean ignoreRange, double xCoord, double yCoord, double zCoord, double xSpeed, double ySpeed, double zSpeed, int[] parameters, CallbackInfo ci)
+    @SuppressWarnings("OverwriteModifiers")
+    @Overwrite
+    public void spawnParticle(int particleID, boolean ignoreRange, double xCoord, double yCoord, double zCoord, double xSpeed, double ySpeed, double zSpeed, int... parameters)
     {
         EnumParticleTypes particle = Objects.requireNonNull(EnumParticleTypes.getParticleFromId(particleID));
         if (particle == EnumParticleTypes.SPELL_MOB || particle == EnumParticleTypes.SPELL_MOB_AMBIENT || !UTConfigBugfixes.MISC.utParticleSpawningToggle) return;
-        if (parameters.length == particle.getArgumentCount()) ((UTServerWorldEventHandlerAccessor) this).getWorld().spawnParticle(particle, xCoord, yCoord, zCoord, 0, xSpeed, ySpeed, zSpeed, 1.0, parameters);
+        if (parameters.length == particle.getArgumentCount()) world.spawnParticle(particle, xCoord, yCoord, zCoord, 0, xSpeed, ySpeed, zSpeed, 1.0, parameters);
     }
 }
