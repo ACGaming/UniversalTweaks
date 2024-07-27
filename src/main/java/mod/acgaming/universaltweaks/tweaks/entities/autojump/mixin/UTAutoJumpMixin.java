63d94536e057fc7611eae0b75807a0e5cda3829b
jchung01 <jjchung276@gmail.com>
2024-07-27 15:31:11 -0700

    Make auto-jump replacement a bit more compatible

--- src/main/java/mod/acgaming/universaltweaks/tweaks/entities/autojump/mixin/UTAutoJumpMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/entities/autojump/mixin/UTAutoJumpMixin.java
@@ -3,10 +3,9 @@ package mod.acgaming.universaltweaks.tweaks.entities.autojump.mixin;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 
-import mod.acgaming.universaltweaks.UniversalTweaks;
-import mod.acgaming.universaltweaks.config.UTConfigGeneral;
 import mod.acgaming.universaltweaks.config.UTConfigTweaks;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -15,13 +14,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(EntityPlayerSP.class)
 public class UTAutoJumpMixin
 {
+    @Unique
+    private boolean ut$stepAssisted;
+
     @Inject(method = "isAutoJumpEnabled", at = @At("RETURN"), cancellable = true)
     public void utAutoJump(CallbackInfoReturnable<Boolean> cir)
     {
         if (!UTConfigTweaks.ENTITIES.utAutoJumpToggle || !Minecraft.getMinecraft().isSingleplayer()) return;
-        if (UTConfigGeneral.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTAutoJump ::: Auto jump check");
-        if (Minecraft.getMinecraft().gameSettings.autoJump && !Minecraft.getMinecraft().player.isSneaking()) Minecraft.getMinecraft().player.stepHeight = 1.2f;
-        else Minecraft.getMinecraft().player.stepHeight = 0.6f;
+        if (Minecraft.getMinecraft().gameSettings.autoJump && Minecraft.getMinecraft().player.stepHeight < 1.0f)
+        {
+            ut$stepAssisted = true;
+            if (!Minecraft.getMinecraft().player.isSneaking()) Minecraft.getMinecraft().player.stepHeight = 1.25f;
+        }
+        else if (ut$stepAssisted)
+        {
+            ut$stepAssisted = false;
+            Minecraft.getMinecraft().player.stepHeight = Math.max(Minecraft.getMinecraft().player.stepHeight - 0.65f, 0.6f);
+        }
         cir.setReturnValue(false);
     }
 }
