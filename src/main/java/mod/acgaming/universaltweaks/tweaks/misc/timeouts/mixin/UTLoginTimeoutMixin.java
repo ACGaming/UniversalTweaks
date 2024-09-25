5a6977ec7a79f1d5e976be6c7eca8c7ceada97b2
jchung01 <jjchung276@gmail.com>
2024-09-24 20:20:15 -0700

    Add network timeout tweaks from RandomPatches
    - Excludes KeepAlive interval tweak as it doesn't seem necessary

---
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/misc/timeouts/mixin/UTLoginTimeoutMixin.java
@@ -0,0 +1,18 @@
+package mod.acgaming.universaltweaks.tweaks.misc.timeouts.mixin;
+
+import net.minecraft.server.network.NetHandlerLoginServer;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+// Courtesy of jchung01, TheRandomLabs (RandomPatches)
+@Mixin(value = NetHandlerLoginServer.class)
+public class UTLoginTimeoutMixin
+{
+    @ModifyConstant(method = "update", constant = @Constant(intValue = 600))
+    private int utModifyLoginTimeout(int original)
+    {
+        return 1800;
+    }
+}
---
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/misc/timeouts/mixin/UTServerReadTimeoutMixin.java
@@ -0,0 +1,33 @@
+package mod.acgaming.universaltweaks.tweaks.misc.timeouts.mixin;
+
+import net.minecraft.network.NetHandlerPlayServer;
+import net.minecraft.util.text.ITextComponent;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Slice;
+
+// Courtesy of jchung01, TheRandomLabs (RandomPatches)
+@Mixin(value = NetHandlerPlayServer.class)
+public class UTServerReadTimeoutMixin
+{
+    // lastPingTime, not sure why this is MCP name
+    @Shadow
+    private long field_194402_f;
+
+    @WrapOperation(method = "update",
+        slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=keepAlive"),
+            to = @At(value = "INVOKE", target = "Lnet/minecraft/profiler/Profiler;endSection()V")),
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetHandlerPlayServer;disconnect(Lnet/minecraft/util/text/ITextComponent;)V"))
+    private void utKeepAliveUntilReadTimeout(NetHandlerPlayServer instance, ITextComponent textComponent, Operation<Void> original, @Local long currentTimeMillis)
+    {
+        if (currentTimeMillis - field_194402_f >= 90)
+        {
+            original.call(instance, textComponent);
+        }
+    }
+}
