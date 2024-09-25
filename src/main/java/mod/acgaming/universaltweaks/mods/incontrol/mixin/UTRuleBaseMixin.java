dfe4aab5a4eaa1eb4e0a2451664986a419c867ab
jchung01 <jjchung276@gmail.com>
2024-09-24 21:42:35 -0700

    Add credits

--- src/main/java/mod/acgaming/universaltweaks/mods/incontrol/mixin/UTRuleBaseMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/mods/incontrol/mixin/UTRuleBaseMixin.java
@@ -7,6 +7,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+// Courtesy of jchung01, McJty
 @Mixin(value = RuleBase.class, remap = false)
 public class UTRuleBaseMixin
 {
--- src/main/java/mod/acgaming/universaltweaks/mods/incontrol/mixin/UTSummonAidRuleMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/mods/incontrol/mixin/UTSummonAidRuleMixin.java
@@ -11,6 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+// Courtesy of jchung01
 @Mixin(value = SummonAidRule.class, remap = false)
 public abstract class UTSummonAidRuleMixin extends RuleBase<SummonEventGetter>
 {
