6c25739fd1a6a4062f0c6bf95647cf5aebdc62b3
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2025-04-07 20:15:02 -0700

    use NarratorDummy object at init

--- src/main/java/mod/acgaming/universaltweaks/tweaks/misc/narrator/mixin/UTNarratorMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/misc/narrator/mixin/UTNarratorMixin.java
@@ -4,6 +4,10 @@ import net.minecraft.client.gui.chat.NarratorChatListener;
 import net.minecraft.util.text.ChatType;
 import net.minecraft.util.text.ITextComponent;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.mojang.text2speech.Narrator;
+import com.mojang.text2speech.NarratorDummy;
 import mod.acgaming.universaltweaks.config.UTConfigTweaks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,6 +18,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(NarratorChatListener.class)
 public class UTNarratorMixin
 {
+    /**
+     * @author WaitingIdly
+     * @reason disable the initialization of the narrator and the narrator thread by redirecting to use {@link NarratorDummy}
+     */
+    @WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "Lcom/mojang/text2speech/Narrator;getNarrator()Lcom/mojang/text2speech/Narrator;", remap = false))
+    private Narrator utUseDummyNarrator(Operation<Narrator> original)
+    {
+        if (!UTConfigTweaks.MISC.utDisableNarratorToggle) return original.call();
+        return new NarratorDummy();
+    }
+
     @Inject(method = "say", at = @At("HEAD"), cancellable = true)
     public void utNarratorSay(ChatType chatTypeIn, ITextComponent message, CallbackInfo ci)
     {
