e11f1e6d95bbde2c4371ec55a8e72e3c41d44ba0
ACGaming <4818419+ACGaming@users.noreply.github.com>
2025-06-15 12:05:23 +0200

    Disable CoFH World remapping

--- src/main/java/mod/acgaming/universaltweaks/mods/cofhworld/mixin/UTCoFHWorldDistributionMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/mods/cofhworld/mixin/UTCoFHWorldDistributionMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Distribution.class)
+@Mixin(value = Distribution.class, remap = false)
 public class UTCoFHWorldDistributionMixin
 {
     @Inject(method = "generateFeature(Ljava/util/Random;IILnet/minecraft/world/World;ZZ)Z", at = @At("HEAD"), cancellable = true)
