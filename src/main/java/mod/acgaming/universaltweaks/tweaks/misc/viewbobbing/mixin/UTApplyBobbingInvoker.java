def25db991250c698ebdb2bf7736594b64490e9d
Apis035 <gd.apis035@gmail.com>
2025-10-02 09:15:28 +0700

    Fix unimplemented interface error

--- src/main/java/mod/acgaming/universaltweaks/tweaks/misc/viewbobbing/mixin/UTApplyBobbingInvoker.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/misc/viewbobbing/mixin/UTApplyBobbingInvoker.java
@@ -9,5 +9,5 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 public interface UTApplyBobbingInvoker
 {
     @Invoker("applyBobbing")
-    void applyBobbing(float partialTicks);
+    void invokeApplyBobbing(float partialTicks);
 }
