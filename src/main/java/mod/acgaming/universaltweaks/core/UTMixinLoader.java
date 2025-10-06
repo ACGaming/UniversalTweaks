4e616b474d9cc5c09125060319ff757dfbda6ce3
ACGaming <4818419+ACGaming@users.noreply.github.com>
2025-10-06 09:26:10 +0200

    Remove Thaumcraft support
    In favor of https://github.com/TheCodex6824/ThaumcraftFix

--- src/main/java/mod/acgaming/universaltweaks/core/UTMixinLoader.java
+++ src/main/java/mod/acgaming/universaltweaks/core/UTMixinLoader.java
@@ -46,7 +46,6 @@ public class UTMixinLoader implements ILateMixinLoader
                 put("mixins.mods.roost.json", () -> loaded("roost") && loaded("contenttweaker"));
                 put("mixins.mods.storagedrawers.client.json", () -> loaded("storagedrawers"));
                 put("mixins.mods.tconstruct.client.json", () -> regularTConLoaded() && UTConfigMods.TINKERS_CONSTRUCT.utParticleFixesToggle);
-                put("mixins.mods.thaumcraft.entities.client.json", () -> loaded("thaumcraft"));
                 put("mixins.tweaks.misc.recipebook.betweenlands.client.json", () -> loaded("thebetweenlands") && UTConfigTweaks.MISC.utRecipeBookToggle);
                 put("mixins.tweaks.misc.recipebook.buildcraft.client.json", () -> loaded("buildcraftcore") && UTConfigTweaks.MISC.utRecipeBookToggle);
             }
@@ -151,9 +150,6 @@ public class UTMixinLoader implements ILateMixinLoader
                 put("mixins.mods.tconstruct.toolcustomization.plustic.json", () -> regularTConLoaded() && loaded("plustic") && UTConfigMods.TINKERS_CONSTRUCT.TOOL_CUSTOMIZATION.utTConToolCustomizationToggle);
                 put("mixins.mods.techreborn.json", () -> loaded("techreborn"));
                 put("mixins.mods.testdummy.copyarmor.json", () -> loaded("testdummy") && UTConfigMods.TEST_DUMMY.utCopyArmor);
-                put("mixins.mods.thaumcraft.foci.focuseffects.json", () -> loaded("thaumcraft"));
-                put("mixins.mods.thaumcraft.foci.focusmediums.json", () -> loaded("thaumcraft"));
-                put("mixins.mods.thaumcraft.json", () -> loaded("thaumcraft"));
                 put("mixins.mods.thefarlanders.dupes.json", () -> loaded("farlanders") && UTConfigMods.THE_FARLANDERS.utDuplicationFixesToggle);
                 put("mixins.mods.thermalexpansion.dupes.json", () -> loaded("thermalexpansion") && UTConfigMods.THERMAL_EXPANSION.utDuplicationFixesToggle);
                 put("mixins.mods.thermalexpansion.json", () -> loaded("thermalexpansion"));
