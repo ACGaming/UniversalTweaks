c4ba362460f7f8e3aba1e9a982703a63b660bdfe
ACGaming <4818419+ACGaming@users.noreply.github.com>
2025-10-25 12:47:18 +0200

    Adjust CoFH Vorpal mixin

--- src/main/java/mod/acgaming/universaltweaks/mods/cofhcore/mixin/UTCoFHVorpalMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/mods/cofhcore/mixin/UTCoFHVorpalMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = EventHandler.class, remap = false)
-public class UTCoFHVorpalMixin
+public abstract class UTCoFHVorpalMixin
 {
     @Redirect(method = "handleLivingHurtEvent", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/event/entity/living/LivingHurtEvent;setAmount(F)V"))
     public void utCoFHVorpalDamage(LivingHurtEvent event, float amount)
