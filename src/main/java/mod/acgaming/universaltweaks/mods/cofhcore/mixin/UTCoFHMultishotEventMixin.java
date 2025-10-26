b371a2455da377bf6a9de7bb758a1b0ae45a57ab
ACGaming <4818419+ACGaming@users.noreply.github.com>
2025-10-26 10:25:50 +0100

    Improve CoFH multishot enchantment tweak
    xJon/Hexxit-II#276

--- src/main/java/mod/acgaming/universaltweaks/mods/cofhcore/mixin/UTCoFHMultishotEventMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/mods/cofhcore/mixin/UTCoFHMultishotEventMixin.java
@@ -1,10 +1,12 @@
 package mod.acgaming.universaltweaks.mods.cofhcore.mixin;
 
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
 
+import cofh.core.init.CoreEnchantments;
 import cofh.core.proxy.EventHandler;
 import mod.acgaming.universaltweaks.config.UTConfigMods;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,14 +19,14 @@ public abstract class UTCoFHMultishotEventMixin
     @Redirect(method = "handleArrowLooseEvent", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getItem()Lnet/minecraft/item/Item;", ordinal = 0))
     public Item utCoFHMultishotArrowLoose(ItemStack stack)
     {
-        if (UTConfigMods.COFH_CORE.utCoFHMultishotToggle && stack.getItem() instanceof ItemBow) return Items.BOW;
+        if (UTConfigMods.COFH_CORE.utCoFHMultishotToggle && stack.getItem() instanceof ItemBow && EnchantmentHelper.getEnchantmentLevel(CoreEnchantments.multishot, stack) > 0) return Items.BOW;
         return stack.getItem();
     }
 
     @Redirect(method = "handleArrowNockEvent", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getItem()Lnet/minecraft/item/Item;", ordinal = 0))
     public Item utCoFHMultishotArrowNock(ItemStack stack)
     {
-        if (UTConfigMods.COFH_CORE.utCoFHMultishotToggle && stack.getItem() instanceof ItemBow) return Items.BOW;
+        if (UTConfigMods.COFH_CORE.utCoFHMultishotToggle && stack.getItem() instanceof ItemBow && EnchantmentHelper.getEnchantmentLevel(CoreEnchantments.multishot, stack) > 0) return Items.BOW;
         return stack.getItem();
     }
 }
