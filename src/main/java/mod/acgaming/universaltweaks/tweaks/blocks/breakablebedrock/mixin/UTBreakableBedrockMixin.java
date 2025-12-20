5706f2d425ebb1a7df18ce3fa233b8617e9ee6ad
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2025-12-19 22:24:13 -0800

    change breakable bedrock to not need at

--- src/main/java/mod/acgaming/universaltweaks/tweaks/blocks/breakablebedrock/mixin/UTBreakableBedrockMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/blocks/breakablebedrock/mixin/UTBreakableBedrockMixin.java
@@ -4,16 +4,17 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Block.class)
 public class UTBreakableBedrockMixin
 {
-    @Redirect(method = "registerBlocks", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;registerBlock(ILjava/lang/String;Lnet/minecraft/block/Block;)V", ordinal = 6))
-    private static void utBreakableBedrock(int id, String textualID, Block block_)
+    @WrapOperation(method = "registerBlocks", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;registerBlock(ILjava/lang/String;Lnet/minecraft/block/Block;)V", ordinal = 6))
+    private static void utBreakableBedrock(int id, String textualID, Block block_, Operation<Void> original)
     {
-        Block.registerBlock(id, textualID, (new Block(Material.ROCK)).setHardness(200.0F).setResistance(6000000.0F).setTranslationKey(textualID).setCreativeTab(CreativeTabs.BUILDING_BLOCKS));
+        original.call(id, textualID, new Block(Material.ROCK).setHardness(200.0F).setResistance(6000000.0F).setTranslationKey(textualID).setCreativeTab(CreativeTabs.BUILDING_BLOCKS));
     }
 }
