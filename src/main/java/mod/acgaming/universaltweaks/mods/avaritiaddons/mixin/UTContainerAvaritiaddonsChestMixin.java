8df1914499b10304b98c588e7bc8eb3da2406133
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2025-12-29 09:47:42 -0800

    avaritiaddons container fix

---
+++ src/main/java/mod/acgaming/universaltweaks/mods/avaritiaddons/mixin/UTContainerAvaritiaddonsChestMixin.java
@@ -0,0 +1,32 @@
+package mod.acgaming.universaltweaks.mods.avaritiaddons.mixin;
+
+import net.minecraft.entity.player.EntityPlayer;
+
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import wanion.avaritiaddons.block.chest.ContainerAvaritiaddonsChest;
+import wanion.lib.common.WContainer;
+import wanion.lib.common.WTileEntity;
+
+// Courtesy of WaitingIdly
+@Mixin(value = ContainerAvaritiaddonsChest.class, remap = false)
+public abstract class UTContainerAvaritiaddonsChestMixin extends WContainer<WTileEntity>
+{
+    private UTContainerAvaritiaddonsChestMixin(@NotNull WTileEntity wTileEntity)
+    {
+        super(wTileEntity);
+    }
+
+    /**
+     * @author WaitingIdly
+     * @reason Ensure the held itemstack is properly handled when the inventory is closed.
+     */
+    @Inject(method = "onContainerClosed", at = @At(value = "HEAD"))
+    private void utCallSuper(EntityPlayer playerIn, CallbackInfo ci)
+    {
+        super.onContainerClosed(playerIn);
+    }
+}
