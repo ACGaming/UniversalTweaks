c1e068d852986d971dfda6d505c40ac80e7bffb7
ACGaming <4818419+ACGaming@users.noreply.github.com>
2026-03-09 21:52:53 +0100

    Apply Simple Difficulty Always Drink tweak to canteens

--- src/main/java/mod/acgaming/universaltweaks/mods/simpledifficulty/mixin/UTAlwaysDrinkMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/mods/simpledifficulty/mixin/UTAlwaysDrinkMixin.java
@@ -1,13 +1,15 @@
 package mod.acgaming.universaltweaks.mods.simpledifficulty.mixin;
 
 import com.charles445.simpledifficulty.api.thirst.IThirstCapability;
+import com.charles445.simpledifficulty.item.ItemCanteen;
+import com.charles445.simpledifficulty.item.ItemDragonCanteen;
 import com.charles445.simpledifficulty.item.ItemDrinkBase;
 import mod.acgaming.universaltweaks.config.UTConfigMods;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(value = ItemDrinkBase.class, remap = false)
+@Mixin(value = {ItemDrinkBase.class, ItemCanteen.class, ItemDragonCanteen.class}, remap = false)
 public abstract class UTAlwaysDrinkMixin
 {
     @Redirect(method = "onItemRightClick", at = @At(value = "INVOKE", target = "Lcom/charles445/simpledifficulty/api/thirst/IThirstCapability;isThirsty()Z"))
