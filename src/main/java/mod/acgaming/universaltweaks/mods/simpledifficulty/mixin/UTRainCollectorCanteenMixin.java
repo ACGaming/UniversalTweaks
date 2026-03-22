b29616059fd18debf9604c661a6bd00e0913c93e
ACGaming <4818419+ACGaming@users.noreply.github.com>
2026-03-22 23:42:13 +0100

    Improve Iron Canteen Interaction Fix

--- src/main/java/mod/acgaming/universaltweaks/mods/simpledifficulty/mixin/UTRainCollectorCanteenMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/mods/simpledifficulty/mixin/UTRainCollectorCanteenMixin.java
@@ -1,59 +1,22 @@
 package mod.acgaming.universaltweaks.mods.simpledifficulty.mixin;
 
-import net.minecraft.block.properties.PropertyInteger;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
+import com.charles445.simpledifficulty.api.SDItems;
 import com.charles445.simpledifficulty.api.item.IItemCanteen;
-import com.charles445.simpledifficulty.api.thirst.ThirstEnum;
 import com.charles445.simpledifficulty.block.BlockRainCollector;
-import com.charles445.simpledifficulty.item.ItemCanteen;
-import com.charles445.simpledifficulty.util.SoundUtil;
-import mod.acgaming.universaltweaks.UniversalTweaks;
-import mod.acgaming.universaltweaks.config.UTConfigGeneral;
-import mod.acgaming.universaltweaks.config.UTConfigMods;
-import org.spongepowered.asm.mixin.Final;
+import com.llamalad7.mixinextras.sugar.Local;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = BlockRainCollector.class, remap = false)
 public abstract class UTRainCollectorCanteenMixin
 {
-    @Shadow
-    @Final
-    public static PropertyInteger LEVEL;
-
-    @Shadow
-    public abstract void setWaterLevel(World world, BlockPos pos, IBlockState state, int level);
-
-    @Inject(method = "onBlockActivated", at = @At(value = "RETURN", ordinal = 4), cancellable = true, remap = true)
-    public void utRainCollectorCanteen(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ, CallbackInfoReturnable<Boolean> cir)
+    @Redirect(method = "onBlockActivated", at = @At(value = "FIELD", target = "Lcom/charles445/simpledifficulty/api/SDItems;canteen:Lnet/minecraft/item/Item;", opcode = Opcodes.GETSTATIC))
+    private Item utRainCollectorCanteen(@Local(name = "item") Item item)
     {
-        if (!UTConfigMods.SIMPLE_DIFFICULTY.utRainCollectorCanteenToggle) return;
-        if (UTConfigGeneral.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTRainCollectorCanteen ::: On block activated");
-        ItemStack itemStack = player.getHeldItem(hand);
-        Item item = itemStack.getItem();
-        int amount = state.getValue(LEVEL);
-        if (item instanceof ItemCanteen && amount > 0 && !world.isRemote && !player.capabilities.isCreativeMode)
-        {
-            IItemCanteen canteen = (IItemCanteen) item;
-            canteen.getThirstEnum(itemStack);
-            if (canteen.tryAddDose(itemStack, ThirstEnum.PURIFIED))
-            {
-                this.setWaterLevel(world, pos, state, amount - 1);
-                SoundUtil.serverPlayBlockSound(world, pos, SoundEvents.ITEM_BUCKET_FILL);
-            }
-            cir.setReturnValue(true);
-        }
+        return (item instanceof IItemCanteen) ? item : SDItems.canteen;
     }
 }
