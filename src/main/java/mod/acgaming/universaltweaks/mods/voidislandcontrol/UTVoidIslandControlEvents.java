2b15bbb5270cb44daba0c4670f9015e3c2fdea44
Irgendwer <67506320+Irgendwer01@users.noreply.github.com>
2026-04-11 00:41:58 +0200

    Review

--- src/main/java/mod/acgaming/universaltweaks/mods/voidislandcontrol/UTVoidIslandControlEvents.java
+++ src/main/java/mod/acgaming/universaltweaks/mods/voidislandcontrol/UTVoidIslandControlEvents.java
@@ -5,23 +5,25 @@ import com.bartz24.voidislandcontrol.api.IslandManager;
 import com.bartz24.voidislandcontrol.api.IslandPos;
 import com.bartz24.voidislandcontrol.config.ConfigOptions;
 
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.math.BlockPos;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraft.world.World;
+import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
 public class UTVoidIslandControlEvents
 {
     @SubscribeEvent
-    public void onPlayerChangedDimension(EntityJoinWorldEvent event)
+    public void onPlayerChangedDimension(PlayerEvent.PlayerChangedDimensionEvent event)
     {
-        if (event.getWorld().isRemote && !(event.getEntity() instanceof EntityPlayer)) return;
-
-        if (event.getWorld().provider.getDimension() == ConfigOptions.worldGenSettings.baseDimension && IslandManager.CurrentIslandsList.isEmpty())
+        if (event.toDim == ConfigOptions.worldGenSettings.baseDimension && IslandManager.CurrentIslandsList.isEmpty())
         {
+            World world = DimensionManager.getWorld(event.toDim);
+            BlockPos pos = new BlockPos(0, ConfigOptions.islandSettings.islandYLevel, 0);
             IslandManager.CurrentIslandsList.add(new IslandPos(0, 0));
-            event.getWorld().setSpawnPoint(new BlockPos(0, 91, 0));
-            EventHandler.createSpawn((EntityPlayer) event.getEntity(), event.getWorld(), new BlockPos(0, 88, 0));
+            world.setSpawnPoint(new BlockPos(0, ConfigOptions.islandSettings.islandYLevel, 0));
+            EventHandler.createSpawn(event.player, world, pos);
+            IslandManager.tpPlayerToPos(event.player, pos, IslandManager.CurrentIslandsList.get(0));
         }
     }
 }
