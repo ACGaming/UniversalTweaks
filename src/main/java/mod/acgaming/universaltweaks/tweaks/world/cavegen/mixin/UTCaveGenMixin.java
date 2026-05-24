a55bfca78265bd31f2f79a2a26dad116b46dcdc2
inboundbark <inboundbark49@gmail.com>
2026-05-24 13:28:04 +0200

    Fix CaveGenSize tweak not working properly
    This should make cave generation identical to 1.6 when values 40 and 15
    are used

--- src/main/java/mod/acgaming/universaltweaks/tweaks/world/cavegen/mixin/UTCaveGenMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/world/cavegen/mixin/UTCaveGenMixin.java
@@ -5,19 +5,23 @@ import net.minecraft.world.gen.MapGenCaves;
 import mod.acgaming.universaltweaks.config.UTConfigTweaks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
 // Courtesy of brunnh1lde
 @Mixin(MapGenCaves.class)
 public abstract class UTCaveGenMixin
 {
-    @ModifyArg(method = "recursiveGenerate", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I", ordinal = 2))
+    @ModifyExpressionValue(
+        method = "recursiveGenerate",
+        at = @At(value = "CONSTANT", args = "intValue=15", ordinal = 0))
     public int utCaveGenSize(int bound)
     {
         return UTConfigTweaks.WORLD.CAVE_GEN.utCaveGenSize;
     }
 
-    @ModifyArg(method = "recursiveGenerate", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I", ordinal = 3))
+    @ModifyExpressionValue(
+        method = "recursiveGenerate",
+        at = @At(value = "CONSTANT", args = "intValue=7", ordinal = 0))
     public int utCaveGenRarity(int bound)
     {
         return UTConfigTweaks.WORLD.CAVE_GEN.utCaveGenRarity;
