4732258ef9c8db1daf6802347266a085bf953cb2
RuiXuqi <90179819+RuiXuqi@users.noreply.github.com>
2026-08-28 11:12:35 +0800

    Clean up UTToggleCheats

--- src/main/java/mod/acgaming/universaltweaks/tweaks/misc/buttons/cheats/UTToggleCheats.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/misc/buttons/cheats/UTToggleCheats.java
@@ -1,12 +1,10 @@
 package mod.acgaming.universaltweaks.tweaks.misc.buttons.cheats;
 
-import java.util.List;
-import java.util.Optional;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiIngameMenu;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.server.integrated.IntegratedServer;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.client.event.GuiScreenEvent;
@@ -15,19 +13,23 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 // Courtesy of modmuss50
 public class UTToggleCheats
 {
-    private static GuiButton cheatsButton;
+    private static final int ID = 101;
 
     @SubscribeEvent
-    public static void utToggleCheatsInitGUI(GuiScreenEvent.InitGuiEvent event)
+    public static void utToggleCheatsInitGUI(GuiScreenEvent.InitGuiEvent.Post event)
     {
         if (!isEscMenuSP(event.getGui())) return;
+        // Tweak Open To LAN width
+        for (GuiButton button : event.getButtonList())
+        {
+            if (button.id == 7)
+            {
+                button.width = 98;
+                break;
+            }
+        }
         GuiScreen gui = event.getGui();
-        Optional<GuiButton> optionalButton = getButton(event.getButtonList(), 7);
-        if (!optionalButton.isPresent()) return;
-        GuiButton openToLAN = optionalButton.get();
-        openToLAN.width = 98;
-        GuiButton toggleCheats = new GuiButton(101, gui.width / 2 + 2, gui.height / 4 + 56, 98, 20, areCheatsEnabled() ? new TextComponentTranslation("btn.universaltweaks.cheats.enabled").getFormattedText() : new TextComponentTranslation("btn.universaltweaks.cheats.disabled").getFormattedText());
-        cheatsButton = toggleCheats;
+        GuiButton toggleCheats = new GuiButton(ID, gui.width / 2 + 2, gui.height / 4 + 56, 98, 20, toggleText(areCheatsEnabled()));
         event.getButtonList().add(toggleCheats);
     }
 
@@ -35,15 +37,17 @@ public class UTToggleCheats
     public static void utToggleCheatsActionPerformed(GuiScreenEvent.ActionPerformedEvent.Post event)
     {
         if (!isEscMenuSP(event.getGui())) return;
-        if (event.getButton().id == 101)
+        GuiButton button = event.getButton();
+        if (button.id == ID)
         {
             toggleCheats(!areCheatsEnabled());
-            cheatsButton.displayString = areCheatsEnabled() ? new TextComponentTranslation("btn.universaltweaks.cheats.enabled").getFormattedText() : new TextComponentTranslation("btn.universaltweaks.cheats.disabled").getFormattedText();
-            event.getGui().mc.player.sendStatusMessage(areCheatsEnabled() ? new TextComponentTranslation("msg.universaltweaks.cheats.enabled") : new TextComponentTranslation("msg.universaltweaks.cheats.disabled"), true);
+            boolean enabled = areCheatsEnabled();
+            button.displayString = toggleText(enabled);
+            Minecraft.getMinecraft().player.sendStatusMessage(enabled ? new TextComponentTranslation("msg.universaltweaks.cheats.enabled") : new TextComponentTranslation("msg.universaltweaks.cheats.disabled"), true);
         }
     }
 
-    public static void toggleCheats(boolean enabled)
+    private static void toggleCheats(boolean enabled)
     {
         IntegratedServer integratedServer = Minecraft.getMinecraft().getIntegratedServer();
         if (integratedServer != null)
@@ -54,19 +58,19 @@ public class UTToggleCheats
         }
     }
 
-    public static boolean areCheatsEnabled()
+    private static boolean areCheatsEnabled()
     {
         return Minecraft.getMinecraft().player.getPermissionLevel() == 4;
     }
 
-    public static Optional<GuiButton> getButton(List<GuiButton> buttonList, int id)
-    {
-        return buttonList.stream().filter(guiButton -> guiButton.id == id).findFirst();
-    }
-
-    public static boolean isEscMenuSP(GuiScreen guiScreen)
+    private static boolean isEscMenuSP(GuiScreen guiScreen)
     {
         if (guiScreen == null || !guiScreen.mc.isSingleplayer()) return false;
         return guiScreen instanceof GuiIngameMenu;
+    }
+
+    private static String toggleText(boolean value)
+    {
+        return I18n.format("btn.universaltweaks.cheats") + " " + I18n.format(value ? "options.on" : "options.off");
     }
 }
