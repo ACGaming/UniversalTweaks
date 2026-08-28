be3a0172b4efb3ea09600bc589181580bb612365
RuiXuqi <90179819+RuiXuqi@users.noreply.github.com>
2026-08-28 09:34:38 +0800

    Refactor UTGuiShareToLan

--- src/main/java/mod/acgaming/universaltweaks/tweaks/misc/gui/lanserverproperties/mixin/GuiShareToLanAccessor.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/misc/gui/lanserverproperties/mixin/GuiShareToLanAccessor.java
@@ -1,5 +1,6 @@
 package mod.acgaming.universaltweaks.tweaks.misc.gui.lanserverproperties.mixin;
 
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiShareToLan;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,15 +9,6 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 @Mixin(GuiShareToLan.class)
 public interface GuiShareToLanAccessor
 {
-    @Accessor("gameMode")
-    String getGameMode();
-
-    @Accessor("gameMode")
-    void setGameMode(String gameMode);
-
-    @Accessor("allowCheats")
-    boolean getAllowCheats();
-
-    @Accessor("allowCheats")
-    void setAllowCheats(boolean allowCheats);
+    @Accessor("lastScreen")
+    GuiScreen getLastScreen();
 }
