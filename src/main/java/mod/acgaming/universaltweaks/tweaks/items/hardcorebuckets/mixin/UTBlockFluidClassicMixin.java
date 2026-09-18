595060500f1a409f1995d7f4cfbf8e8b1c21dca3
ACGaming <4818419+ACGaming@users.noreply.github.com>
2026-09-18 10:55:53 +0200

    Improve Hardcore Buckets tweak
    Closes #891

--- src/main/java/mod/acgaming/universaltweaks/tweaks/items/hardcorebuckets/mixin/UTBlockFluidClassicMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/items/hardcorebuckets/mixin/UTBlockFluidClassicMixin.java
@@ -1,19 +1,18 @@
 package mod.acgaming.universaltweaks.tweaks.items.hardcorebuckets.mixin;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.item.ItemBucket;
+import net.minecraftforge.fluids.BlockFluidClassic;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ItemBucket.class)
-public abstract class UTItemBucketMixin
+@Mixin(BlockFluidClassic.class)
+public abstract class UTBlockFluidClassicMixin
 {
-    @Redirect(method = "tryPlaceContainedLiquid", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;getDefaultState()Lnet/minecraft/block/state/IBlockState;"))
-    private IBlockState utTryPlaceContainedLiquid(Block block)
+    @Redirect(method = "place", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/fluids/BlockFluidClassic;getDefaultState()Lnet/minecraft/block/state/IBlockState;"))
+    private static IBlockState utPlace(BlockFluidClassic block)
     {
         return block.getDefaultState().withProperty(BlockLiquid.LEVEL, 1);
     }
--- src/main/java/mod/acgaming/universaltweaks/tweaks/items/hardcorebuckets/mixin/UTBlockLiquidMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/items/hardcorebuckets/mixin/UTBlockLiquidMixin.java
@@ -3,17 +3,20 @@ package mod.acgaming.universaltweaks.tweaks.items.hardcorebuckets.mixin;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.item.ItemBucket;
 
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ItemBucket.class)
-public abstract class UTItemBucketMixin
+// Liquid!!
+// Snake, did you like my sunglasses?
+@Mixin(BlockLiquid.class)
+public abstract class UTBlockLiquidMixin
 {
-    @Redirect(method = "tryPlaceContainedLiquid", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;getDefaultState()Lnet/minecraft/block/state/IBlockState;"))
-    private IBlockState utTryPlaceContainedLiquid(Block block)
+    @Dynamic("Method added by Fluidlogged API via ASM")
+    @Redirect(method = "place", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;getDefaultState()Lnet/minecraft/block/state/IBlockState;"))
+    private static IBlockState utPlace(Block block)
     {
         return block.getDefaultState().withProperty(BlockLiquid.LEVEL, 1);
     }
