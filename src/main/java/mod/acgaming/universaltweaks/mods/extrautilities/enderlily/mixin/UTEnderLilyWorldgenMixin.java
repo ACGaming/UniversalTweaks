1a7d3a210494abfbcdb5d6820975e37fda8fb422
kaduvill <jottafanftw@gmail.com>
2026-09-19 21:51:16 +0200

    chunk.getWorld().isOutsideBuildHeight(pos) change

--- src/main/java/mod/acgaming/universaltweaks/mods/extrautilities/enderlily/mixin/UTEnderLilyWorldgenMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/mods/extrautilities/enderlily/mixin/UTEnderLilyWorldgenMixin.java
@@ -4,9 +4,7 @@ import com.rwtema.extrautils2.worldgen.SingleChunkGen;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.chunk.Chunk;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,14 +12,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(value = SingleChunkGen.class, remap = false)
 public abstract class UTEnderLilyWorldgenMixin
 {
-    @Shadow
-    @Final
-    public String name;
-
     @Inject(method = "setBlockState", at = @At("HEAD"), cancellable = true)
-    private void utPreventEnderLilyWorldgenCrash(Chunk chunk, BlockPos pos, IBlockState state, CallbackInfo ci)
+    private void utPreventOutOfBoundsWorldgen(Chunk chunk, BlockPos pos, IBlockState state, CallbackInfo ci)
     {
-        if ("EnderLillies".equals(name) && pos.getY() >= 256)
+        if (chunk.getWorld().isOutsideBuildHeight(pos))
         {
             ci.cancel();
         }
